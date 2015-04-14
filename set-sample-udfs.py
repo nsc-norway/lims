bf99d0d176befd41921e4dcbeb2b3463865b3dbd
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2015-04-14 11:41:53 +0200

    script to copy sample udfs from process

---
+++ set-sample-udfs.py
@@ -0,0 +1,23 @@
+import sys
+from genologics.lims import *
+from genologics import config
+
+
+def main(process_id):
+    lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
+    process = Process(lims, id=process_id)
+    
+    for analyte in process.all_inputs(unique=True):
+        sample = analyte.samples[0]
+        
+        for udfname, udfval in process.udf.items():
+            sample.udf['NSC ' + udfname] = udfval
+
+
+if len(sys.argv) == 2:
+    main(sys.argv[1])
+else:
+    print "use: python set-sample-udfs.py PROCESS_ID"
+    sys.exit(1)
+
+
