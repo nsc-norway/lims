bc9701699b6d25c4424d2f239f2b71b14f06bc91
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2015-04-22 17:31:45 +0200

    work on labels.

--- label-printing/label-print.py
+++ label-printing/label-print.py
@@ -1,42 +1,75 @@
 import os
-
+import re
+import uuid
 import tempfile
-import win32api
-import win32print
-
-from flask import Flask, request
-from appy import pod
-app = Flask(__name__)
+import datetime
+import barcode
+from appy.pod.renderer import Renderer
+from hubarcode.datamatrix import DataMatrixEncoder
+from genologics.lims import *
+from genologics import config
 
 #template_dir = "C:\\Users\\admin\\Desktop"
-template_dir = "/Users/paalmbj/Documents"
+template_dir = "/Users/paalmbj/git/lims/label-printing/templates"
+print_spool_dir = "/tmp"
 
+class Barcode(object):
+    def __init__(self, name, type, cellsize):
+        self.name = name
+        self.type = type
+        self.cellsize = cellsize
 
-label_template = {
-        'tube': 'tube.odt',
-        'box': 'box.odt'
-        }
-label_printer = {
-        'tube': 'LABEL1',
-        'box': 'LABEL2'
-        }
 
 use_printer = "LABEL1"
 
-@app.route('/print/<labeltype>', methods=['POST'])
-def print_label(labeltype):
-    template_path = os.path.join(template_dir, label_template[labeltype])
-    temp = tempfile.NamedTemporaryFile(suffix=".odt", delete=False)
-    temp.close()
-    try:
-        renderer = pod.Renderer(template_path, request.post, temp.name)
-        renderer.run()
-        
-        win32api.ShellExecute(
-                0, "print", temp.name,
-                '/d:"%s"' % label_printer[labeltype],
-                ".", 0)
-    finally:
-        os.remove(temp.name)
+def prepare_odt(template, printer, template_parameters):
+    template_path = os.path.join(template_dir, template)
+    output_name = "{0}-{1:%Y%m%d%H%M_%f}.odt".format(
+            printer,
+            datetime.datetime.now()
+            )
+    output_path = os.path.join(print_spool_dir, "transfer", output_name)
+    renderer = Renderer(
+            template_path,
+            template_parameters,
+            output_path,
+            pythonWithUnoPath="/Applications/LibreOffice.app/Contents/MacOS/python"
+            )
+    renderer.run()
+    os.rename(output_path, os.path.join(print_spool_dir, output_name))
+
+
+
+
+def make_tube_label(analyte):
+    sample = analyte.samples[0]
+    project = sample.project
+    project_match = re.match(r"(.*)-(.*)-(\d{4}-\d{2}-\d{2})", project.name)
+    if project_match:
+        project_customer, project_label, project_date = project_match.groups()
+    else:
+        project_customer = "Invalid"
+        project_label = "Project"
+        project_date = "Name"
+
+    params = {}
+    image_data = DataMatrixEncoder(analyte.id).get_imagedata(cellsize=1)
+    params['barcode'] = image_data
+    params['project_customer'] = project_customer
+    params['project_label'] = project_label
+    params['project_date'] = project_date
+
+    params['date'] = datetime.date.today().strftime("%y-%m-%d")
+    params['type'] = 'STOCK'
+    params['location'] = '0001-A1'
+
+    prepare_odt('tube.odt', 'LABEL1', params)
+
  
 
+def main():
+    lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD) 
+    make_tube_label(Artifact(lims, id = "SUN155A1PA1"))
+
+
+main()
