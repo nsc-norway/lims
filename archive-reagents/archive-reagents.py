ec69b6b1c48e22d5cdb33e54976929fccd7a0323
Marius Bjoernstad <paalmbj@dev-lims.ous.nsc.local>
2015-05-28 12:10:12 +0000

    fix bug, should refer to kit type

--- archive-reagents/archive-reagents.py
+++ archive-reagents/archive-reagents.py
@@ -6,10 +6,10 @@ def main(process_id, kit_names):
     lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD) 
     step = Step(lims, id=process_id)
     for lot in step.reagent_lots:
-        if lot.reagent_kit in kit_names:
+        if lot.reagent_kit.name in kit_names:
             lot.status = "ARCHIVED"
             lot.put()
 
 
 if __name__ == "__main__":
-    main(sys.argv[0], sys.argv[1:])
+    main(sys.argv[1], sys.argv[2:])
