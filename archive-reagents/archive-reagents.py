8ec65c56aee8b63e8e435a5e8588d661e7faf71c
Marius Bjoernstad <paalmbj@dev-lims.ous.nsc.local>
2015-05-28 12:54:51 +0000

    changing auto-archive to do all available lots

--- archive-reagents/archive-reagents.py
+++ archive-reagents/archive-reagents.py
@@ -2,14 +2,13 @@ import sys
 from genologics.lims import *
 from genologics import config
 
-def main(process_id, kit_names):
+def main(process_id):
     lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD) 
     step = Step(lims, id=process_id)
     for lot in step.reagent_lots:
-        if lot.reagent_kit.name in kit_names:
-            lot.status = "ARCHIVED"
-            lot.put()
+        lot.status = "ARCHIVED"
+        lot.put()
 
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2:])
+    main(sys.argv[1])
