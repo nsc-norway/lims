95d72ce140821cceccec4eed6377195e7bb8bd96
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2015-06-05 09:36:38 +0200

    started implementing a deployment routine for lims repo

---
+++ deploy/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Deployment script for LIMS repository for OUS site.
+# Deploys the lims and genologics repositories from the dev-lims server onto
+# ous-lims.
+# Arguments:
+#  
+
+
