3b14e4f387138c68e3508eb3ba58cef68d1d5a2f
Marius Bjoernstad <paalmbj@dev-lims.ous.nsc.local>
2015-06-11 16:34:28 +0000

    started dilution calc

--- tapestation/dilution-calculator.py
+++ tapestation/dilution-calculator.py
@@ -1,12 +1,34 @@
 import sys
+import csv
 from genologics.lims import *
 from genologics import config
+
+def get_buffer_vol(input):
+    molarity = input.udf['Molarity']
+    norm_conc = output.udf['Normalized Conc. (nM)']
+
+
 
 def main(process_id, output_file_path):
     lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD) 
     process = Process(lims, id=process_id)
-    outputs = {}
-    # TODO!
+
+
+    with open(output_file_path) as out_file:
+        out = csv.writer(out_file)
+        out.writerow(["Project", "Sample name", "Normalised conc", "Buffer Volume"])
+
+        for i,o in process.input_output_maps:
+            output = o['uri']
+            if o and o['output-type'] == 'ResultFile' and o['output-generation-type'] == 'PerInput': 
+                input = i['uri']
+                
+                
+
+
+            
+    
+
 
 
 main(sys.argv[1], sys.argv[2])
