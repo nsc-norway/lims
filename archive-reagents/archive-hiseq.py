f0ca403d26bcb0680acc2641c39a51d540541404
Marius Bjoernstad <paalmbj@dev-lims.ous.nsc.local>
2015-06-11 12:40:16 +0000

    adding archiving of hiseq reagents

---
+++ archive-reagents/archive-hiseq.py
@@ -0,0 +1,15 @@
+import sys
+from genologics.lims import *
+from genologics import config
+
+def main(process_id):
+    lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD) 
+    step = Step(lims, id=process_id)
+    for lot in step.reagent_lots:
+        if lot.lot_number != "Rapid dummy":
+            lot.status = "ARCHIVED"
+            lot.put()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
