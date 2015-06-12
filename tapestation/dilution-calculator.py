9fcd9d2e42e96c21185ffc58d0c63aa217eaa76e
Marius Bjoernstad <paalmbj@dev-lims.ous.nsc.local>
2015-06-12 15:22:25 +0000

    made a normalisation calculator for libraries

--- tapestation/dilution-calculator.py
+++ tapestation/dilution-calculator.py
@@ -1,34 +1,37 @@
 import sys
 import csv
+import StringIO
 from genologics.lims import *
 from genologics import config
 
-def get_buffer_vol(input):
-    molarity = input.udf['Molarity']
-    norm_conc = output.udf['Normalized Conc. (nM)']
 
+def get_buffer_vol(normalised_concentration, stock_volume, stock_concentration):
+    return stock_volume * (stock_concentration / normalised_concentration - 1.0)
 
-
-def main(process_id, output_file_path):
-    lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD) 
+def main(process_id, output_file_id):
+    lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
     process = Process(lims, id=process_id)
 
+    out_buf = StringIO.StringIO()   
 
-    with open(output_file_path) as out_file:
+    output_file_name = output_file_id + "_norm.csv"
+    with open(output_file_name, 'wb') as out_file:
         out = csv.writer(out_file)
-        out.writerow(["Project", "Sample name", "Normalised conc", "Buffer Volume"])
+        out.writerow(["Project", "Sample name", "Stock conc", "Stock volume", "Normalised conc", "Buffer volume"])
 
         for i,o in process.input_output_maps:
             output = o['uri']
-            if o and o['output-type'] == 'ResultFile' and o['output-generation-type'] == 'PerInput': 
+            if o and o['output-type'] == 'Analyte' and o['output-generation-type'] == 'PerInput':
                 input = i['uri']
-                
-                
+                project_name = input.samples[0].project.name
+                sample_name = input.samples[0].name
 
+                norm_conc = output.udf['Normalized conc. (nM)']
+                stock_vol = output.udf['Volume of stock']
+                stock_conc = input.udf['Concentration']
+                buffer_vol = get_buffer_vol(norm_conc, stock_vol, stock_conc)
 
-            
-    
-
+                out.writerow([project_name, sample_name, stock_conc, stock_vol, norm_conc, buffer_vol])
 
 
 main(sys.argv[1], sys.argv[2])
