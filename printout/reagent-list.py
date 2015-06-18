ed47eb2f1e94cf75b51295750c74a1db2d7a0ad3
Marius Bjoernstad <paalmbj@dev-lims.ous.nsc.local>
2015-06-18 08:16:05 +0000

    just fixing the formatting, still a bug in sorting

--- printout/reagent-list.py
+++ printout/reagent-list.py
@@ -9,7 +9,10 @@ def main(process_id):
     outputs = process.all_outputs(unique=True)
 
     data = [["Sample", "Container", "Well", "Index"]]
-    for o in sorted((o for o in outputs if o.type == "Analyte"), key=lambda x: (x.location[0], reversed(x.location[1]))):
+    for o in sorted(
+            (o for o in outputs if o.type == "Analyte"),
+            key=lambda x: (x.location[0], reversed(x.location[1]))
+            ):
         data.append([o.name, o.location[0].name, o.location[1], next(iter(o.reagent_labels))])
     printtable.print_table("Indexes for project " + o.samples[0].project.name, data)
 
