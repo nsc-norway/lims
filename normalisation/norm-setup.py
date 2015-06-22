32c35fa33f48332e508a493f150d7be6d551639e
Marius Bjoernstad <paalmbj@dev-lims.ous.nsc.local>
2015-06-22 14:16:03 +0000

    gimped version of norm-setup

--- normalisation/norm-setup.py
+++ normalisation/norm-setup.py
@@ -6,36 +6,27 @@ from genologics import config
 
 # Setup script for normalisation:
 # - Copies UDFs from inputs to outputs
-# - Sets default normalised concentration and input volume on each output
+
+# This script has been reduced to a version of the Genologics 
+# copyUDFs function in their ngs-extensions.jar, but I keep it
+# because it's faster.
 
 # use:
-# python norm-setup.py PROCESS-ID INPUT-VOL NORM-CONC {COPY-UDFs}
+# python norm-setup.py PROCESS-ID {COPY-UDFs}
 # 
 # PROCESS-ID: LIMS process ID
-# INPUT-VOL:  Volume to take from inputs
-# NORM-CONC:  Desired normalised concentration. If Molarity of input is set, and is
-#             less, set the normalized conc field equal to the Molarity.
 # COPY-UDFS:  Any number of UDFs to copy from input to output analytes. Will skip if
 #             doesn't exist on source.
 
-def main(process_id, input_vol, norm_conc, copy_udfs):
+def main(process_id, copy_udfs):
     lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
     process = Process(lims, id=process_id)
 
-    input_vol_float = float(input_vol)
-    norm_conc_float = float(norm_conc)
-    
     for i,o in process.input_output_maps:
         if o and o['output-type'] == 'Analyte' and o['output-generation-type'] == 'PerInput':
             output = o['uri']
             input = i['uri']
             output.get()
-            try:
-                source_molarity = input.udf['Molarity']
-                output.udf['Normalized conc. (nM)'] = min(norm_conc_float, source_molarity)
-            except KeyError:
-                output.udf['Normalized conc. (nM)'] = norm_conc_float
-            output.udf['Volume of input'] = input_vol_float
             for u in copy_udfs:
                 try:
                     output.udf[u] = input.udf[u]
@@ -43,5 +34,5 @@ def main(process_id, input_vol, norm_conc, copy_udfs):
                     pass
             output.put()
 
-main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4:])
+main(sys.argv[1], sys.argv[2:])
 
