6972b86640065fbcb2cd15e03c1c7d29298830f5
Marius Bjoernstad <paalmbj@dev-lims.ous.nsc.local>
2015-06-24 13:23:28 +0000

    Making the label printing application into a windows service

--- label-printing/windows-print-service.py
+++ label-printing/windows-print-service.py
@@ -6,6 +6,16 @@ import win32api
 import win32file
 import win32event
 import win32con
+import win32serviceutil
+import win32service
+import win32event
+import servicemanager
+
+# Can be installed as a service by running the following in an administrator shell:
+# python windows-print-service.py install
+
+# Then go into the service manager and set it to start automatically, and run as the
+# Local System user.
 
 print_dir = "C:\\Printing"
 libreoffice_path = "C:\\Program Files (x86)\\LibreOffice 4\\program\\soffice.exe"
@@ -29,25 +39,44 @@ def do_print(filepath):
         os.remove(filepath)
 
 
-def monitor_and_print():
-    # Set up change notification
-    change_handle = win32file.FindFirstChangeNotification(
-            print_dir, 
-            0,
-            win32con.FILE_NOTIFY_CHANGE_FILE_NAME
-            )
+class AppServerSvc (win32serviceutil.ServiceFramework):
+    _svc_name_ = "LabelPrintingService"
+    _svc_display_name_ = "Label printing service"
 
-    for j in get_new_jobs():
-        do_print(j)
-        
-    while True:
-        result = win32event.WaitForSingleObject(change_handle, 500)
-        if result == win32con.WAIT_OBJECT_0:
-            for j in get_new_jobs():
-                do_print(j)
-            win32file.FindNextChangeNotification(change_handle)
+    def __init__(self,args):
+        win32serviceutil.ServiceFramework.__init__(self,args)
+        self.hWaitStop = win32event.CreateEvent(None,0,0,None)
+        self.stop = False
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        self.stop = True
+        win32event.SetEvent(self.hWaitStop)
+
+    def SvcDoRun(self):
+        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
+                              servicemanager.PYS_SERVICE_STARTED,
+                              (self._svc_name_,''))
+        self.monitor_and_print()
+
+    def monitor_and_print(self):
+        # Set up change notification
+        change_handle = win32file.FindFirstChangeNotification(
+                print_dir, 
+                0,
+                win32con.FILE_NOTIFY_CHANGE_FILE_NAME
+                )
+
+        for j in get_new_jobs():
+            do_print(j)
+            
+        while not self.stop:
+            result = win32event.WaitForMultipleObjects([change_handle, self.hWaitStop], False, 500)
+            if result == win32con.WAIT_OBJECT_0:
+                for j in get_new_jobs():
+                    do_print(j)
+                win32file.FindNextChangeNotification(change_handle)
 
 
-monitor_and_print()
-
-
+if __name__ == '__main__':
+    win32serviceutil.HandleCommandLine(AppServerSvc)
