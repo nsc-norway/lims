d98f4443464f74c68766da6c2a0f56b4310700da
Marius Bjoernstad <paalmbj@dev-lims.ous.nsc.local>
2015-06-29 09:04:40 +0000

    special case for None, dont assign to w/f

--- helpers/assign-workflow.py
+++ helpers/assign-workflow.py
@@ -8,9 +8,10 @@ from genologics import config
 def main(process_id, workflow_name):
     lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
     process = Process(lims, id=process_id)
-    workflows = lims.get_workflows(name=workflow_name)
-    workflow = workflows[0]
-    lims.route_analytes(process.all_inputs(unique=True), workflow)
+    if workflow_name != "None":
+        workflows = lims.get_workflows(name=workflow_name)
+        workflow = workflows[0]
+        lims.route_analytes(process.all_inputs(unique=True), workflow)
 
 
 main(sys.argv[1], sys.argv[2])
