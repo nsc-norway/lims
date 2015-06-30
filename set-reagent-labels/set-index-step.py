6c4aa32b7e35f4a150546c6fa8246b540be5ba9a
Marius Bjoernstad <paalmbj@dev-lims.ous.nsc.local>
2015-06-30 12:10:08 +0000

    finished adding "set index" function to a step (project evaluation for now)

--- set-reagent-labels/set-index-step.py
+++ set-reagent-labels/set-index-step.py
@@ -9,6 +9,9 @@ from genologics import config
 from collections import defaultdict
 import sys
 
+CATEGORY_UDF = "Index category (prepared libraries)"
+SAMPLE_INDEX_UDF = "Index requested/used"
+
 lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
 
 def get_all_reagent_types():
@@ -17,7 +20,7 @@ def get_all_reagent_types():
     one)""" 
     # Using a loop similar to Lims._get_instances()
     reagent_types = defaultdict(set)
-    root = lims.get("reagenttypes")
+    root = lims.get(lims.get_uri("reagenttypes"))
     while root:
         for node in root.findall("reagent-type"):
 
@@ -29,71 +32,117 @@ def get_all_reagent_types():
                     for tk in name.split(" ")
                     if tk != ""
                     ):
-                reagent_types[tk].add(uri)
+                reagent_types[token].add(uri)
                 
         node = root.find('next-page')
         root = None
         if not node is None:
-            root = self.get(node.attrib['uri'])
+            root = lims.get(node.attrib['uri'])
 
     return reagent_types
 
 
-def get_candidates_per_analyte(analytes, reagents):
-    # Category indexes: a list for each category. Dict values are lists
-    # of indexes.
+def get_reagents_auto_category(reagents, analytes, sequence_match=False):
     category_indexes = {}
     candidate_categories = set()
+    ana_no_match = [] # list of analytes with no indexes at all
+    not_in_category = []
 
+    # Add all possible categories -- use categories for the first  analyte
     analyte = analytes[0]
-    sequence = analyte.samples[0].udf['Index requested/used']
-    for token, reagent_uri in reagents[sequence]:
-        reagent = Reagent(lims, id=reagent_uri)
+    ana_match_string = analyte.samples[0].udf[SAMPLE_INDEX_UDF]
+    for reagent_uri in reagents[ana_match_string]:
+        reagent = ReagentType(lims, uri=reagent_uri) 
         candidate_categories.add(reagent.category)
-        category_indexes[reagent.category].append(reagent.name)
+        category_indexes[reagent.category] = []
 
-    for analyte in analytes[1:]:
-        sequence = analyte.samples[0].udf['Index requested/used']
-        candidates_not_seen = set(candidate_categories)
-        for token, reagent_uri in reagents[sequence]:
-            reagent = Reagent(lims, id=reagent_uri)
-            if reagent.category in candidate_categories:
-                candidates_not_seen.remove(reagent.category)
+    # 2. Process all analytes including the first one
+    for analyte in analytes:
+        new_candidates = set()
+        ana_match_string = analyte.samples[0].udf[SAMPLE_INDEX_UDF]
+        analyte_reagents = reagents[ana_match_string]
+        if not analyte_reagents:
+            ana_no_match.append(analyte.name)
+        for reagent_uri in reagents[ana_match_string]:
+            reagent = ReagentType(lims, uri=reagent_uri)
+            if not sequence_match or reagent.index_sequence == ana_match_string:
+                if reagent.category in candidate_categories:
+                    new_candidates.add(reagent.category)
+                    category_indexes[reagent.category].append(reagent.name)
+        candidate_categories = new_candidates
 
-    
+    if ana_no_match:
+        print "Samples with no match at all: ", ", ".join(ana_no_match)
+        sys.exit(1)
+    elif len(candidate_categories) == 1:
+        cat = next(iter(candidate_categories))
+        return cat, category_indexes[cat]
+    elif not candidate_categories:
+        print "No reagent category has all the given indexes"
+        sys.exit(1)
+    elif len(candidate_categories) > 1:
+        print "Multiple categories match: ", ", ".join(candidate_categories)
+        sys.exit(1)
 
 
-def select_indexes(analytes, category, search_mode):
+def get_reagents_for_category(reagents, analytes, category, sequence_match=False):
+    match_reagents = []
+    ana_no_match = [] # list of analytes with no indexes at all
+
+    # 2. Process all analytes including the first one
+    for analyte in analytes:
+        ana_match_string = analyte.samples[0].udf[SAMPLE_INDEX_UDF]
+        analyte_reagents = reagents[ana_match_string]
+        match = False
+        for reagent_uri in reagents[ana_match_string]:
+            reagent = ReagentType(lims, uri=reagent_uri)
+            if not sequence_match or reagent.index_sequence == ana_match_string:
+                if reagent.category == category:
+                    match_reagents.append(reagent.name)
+                    match = True
+        if not match:
+            ana_no_match.append(analyte.name)
 
 
+    if ana_no_match:
+        print "No matching reagent type found for samples: ", ", ".join(ana_no_match)
+        sys.exit(1)
+    else:
+        return match_reagents
 
-def main(process_id, category):
+
+def main(process_id):
+    """Assign reagents based on available reagent types, the Index requested/used UDF,
+    and the specified category. If category is not given, any category could be used.
+    """
     process = Process(lims, id=process_id)
 
     reagents = get_all_reagent_types() 
-
     analytes = process.all_inputs()
-    print "Checking the requested indexes..."
-    sequences = set(ana.samples[0].udf['Index requested/used'] for ana in analytes)
 
-    if not sequences.issubset(set(reagents.keys())):
-        print "Requested index not in category for samples: ", ", ".join(
-                ana.samples[0].name for ana in analytes
-                if not ana.samples[0].udf['Index requested/used'] in reagents.keys()
-                )
-        sys.exit(1)
+    try:
+        category = process.udf[CATEGORY_UDF]
+    except KeyError:
+        category = None
 
-    print "Assigning the indexes..."
-    for ana in analytes:
-        sequence = ana.samples[0].udf['Index requested/used']
-        sequences.add(sequence)
-        ana.reagent_labels.clear()
-        ana.reagent_labels.add(reagents[sequence].name)
-        ana.put()
+    if category == "Auto-detect":
+        category, result = get_reagents_auto_category(reagents, analytes)
+    else:
+        result = get_reagents_for_category(reagents, analytes, category)
+
+    process.udf[CATEGORY_UDF] = category
+    process.put()
+
+    # Assign the indexes
+    for analyte, reagent_name in zip(analytes, result):
+        sequence = analyte.samples[0].udf[SAMPLE_INDEX_UDF]
+        analyte.reagent_labels.clear()
+        analyte.reagent_labels.add(reagent_name)
+        analyte.put()
 
     print "Successfully set the indexes"
 
 
 
-main(sys.argv[1], sys.argv[2])
+main(sys.argv[1])
 
