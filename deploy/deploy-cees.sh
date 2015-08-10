5d36c3a915b86e4cd01b67cf7298b6b2bf20a439
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2015-08-10 16:46:31 +0200

    added a deployment script for cees-lims server

---
+++ deploy/deploy-cees.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+# Deployment script for LIMS repository for CEES site.
+# Deploys the lims and genologics repositories from the local repository to
+# cees-lims.
+# Arguments: tag to send
+
+( pushd ../../genologics > /dev/null &&
+   git archive $1 &&
+   popd > /dev/null &&
+   pushd .. > /dev/null &&
+   git archive $1 && 
+   popd > /dev/null ) |
+   	ssh cees-lims.sequencing.uio.no "/bin/bash -c '(pushd /opt/gls/clarity/customextensions > /dev/null &&
+	mv genologics genologics.2 &&
+	mv lims lims.2 &&
+	mkdir genologics lims &&
+	cd genologics &&
+	tar x &&
+	cd ../lims &&
+	tar x && 
+	cd .. &&
+	rm -rf genologics.2 lims.2 )'"
+
+
