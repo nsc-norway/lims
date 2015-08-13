618d16355b18905e15bcc591df47388794f58c17
Marius Bjoernstad <paalmbj@dev-lims.ous.nsc.local>
2015-08-13 12:29:34 +0000

    moved index detection code into library module

---
+++ set-reagent-labels/indexes.py
@@ -0,0 +1,130 @@
+# Library for setting indexes
+
+from genologics.lims import *
+from genologics import config
+from collections import defaultdict
+import sys
+
+SAMPLE_INDEX_UDF = "Index requested/used"
+
+lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
+
+def get_all_reagent_types():
+    """Load all reagent types with name only, from the API resource.
+    (this is a lot faster than getting the full representations one by
+    one).
+    
+    Preaks the name into space-separated tokens, also removing brackets
+    () at the beginning and end of the tokens. Then returns a dict indexed
+    by the token, where the values are sets of reagent types matching that
+    token. {token => (reagent1, reagent2, ...)}
+    
+    Example: The name "AD005 (ACAGTG)" becomes two tokens: AD005 
+    and ACAGTG.
+    """ 
+    # Using a loop similar to Lims._get_instances()
+    reagent_types = defaultdict(set)
+    root = lims.get(lims.get_uri("reagenttypes"))
+    while root:
+        for node in root.findall("reagent-type"):
+
+            # add a reagent type to the search index, indexed by all words
+            name = node.attrib['name']
+            uri = node.attrib['uri']
+            for token in (
+                    tk.strip("()")
+                    for tk in name.split(" ")
+                    if tk != ""
+                    ):
+                reagent_types[token].add(uri)
+                
+        node = root.find('next-page')
+        root = None
+        if not node is None:
+            root = lims.get(node.attrib['uri'])
+
+    return reagent_types
+
+
+def get_reagents_auto_category(reagents, analytes, sequence_match=False):
+    category_indexes = {}
+    candidate_categories = set()
+    ana_no_match = [] # list of analytes with no indexes at all
+    not_in_category = []
+
+    # Add all possible categories -- use categories for the first  analyte
+    analyte = analytes[0]
+    ana_match_string = analyte.samples[0].udf[SAMPLE_INDEX_UDF]
+    for reagent_uri in reagents[ana_match_string]:
+        reagent = ReagentType(lims, uri=reagent_uri) 
+        candidate_categories.add(reagent.category)
+        category_indexes[reagent.category] = []
+
+    # 2. Process all analytes including the first one
+    for analyte in analytes:
+        new_candidates = set()
+        ana_match_string = analyte.samples[0].udf[SAMPLE_INDEX_UDF]
+        analyte_reagents = reagents[ana_match_string]
+        if not analyte_reagents:
+            ana_no_match.append(analyte.name)
+        for reagent_uri in reagents[ana_match_string]:
+            reagent = ReagentType(lims, uri=reagent_uri)
+            if not sequence_match or reagent.index_sequence == ana_match_string:
+                if reagent.category in candidate_categories:
+                    if reagent.category in new_candidates:
+                        print "Ambiguous match for sample", analyte.name, ": in category", reagent.category,\
+                                "the specified index matches multiple reagent types:", reagent.name, "and",\
+                                category_indexes[reagent.category][-1]
+                        sys.exit(-1)
+                    else:
+                        new_candidates.add(reagent.category)
+                        category_indexes[reagent.category].append(reagent.name)
+        candidate_categories = new_candidates
+
+    if ana_no_match:
+        print "Samples with no match at all: ", ", ".join(ana_no_match)
+        sys.exit(1)
+    elif len(candidate_categories) == 1:
+        cat = next(iter(candidate_categories))
+        return cat, category_indexes[cat]
+    elif not candidate_categories:
+        print "No reagent category has all the given indexes"
+        sys.exit(1)
+    elif len(candidate_categories) > 1:
+        print "Multiple categories match: ", ", ".join(candidate_categories)
+        sys.exit(1)
+
+
+def get_reagents_for_category(reagents, analytes, category, sequence_match=False):
+    match_reagents = []
+    ana_no_match = [] # list of analytes with no indexes at all
+
+    # 2. Process all analytes including the first one
+    for analyte in analytes:
+        ana_match_string = analyte.samples[0].udf[SAMPLE_INDEX_UDF]
+        analyte_reagents = reagents[ana_match_string]
+        match = False
+        for reagent_uri in reagents[ana_match_string]:
+            reagent = ReagentType(lims, uri=reagent_uri)
+            if not sequence_match or reagent.index_sequence == ana_match_string:
+                if reagent.category == category:
+                    if match:
+                        print "Ambiguous match for sample", analyte.name, ": specified index "\
+                                "matches multiple reagent types:", reagent.name, "and",\
+                                match_reagents[-1]
+                        sys.exit(-1)
+                    else:
+                        match_reagents.append(reagent.name)
+                        match = True
+        if not match:
+            ana_no_match.append(analyte.name)
+
+
+    if ana_no_match:
+        print "No matching reagent type found for samples: ", ", ".join(ana_no_match)
+        sys.exit(1)
+    else:
+        return match_reagents
+
+
+
