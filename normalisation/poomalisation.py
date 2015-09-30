e12148de13e63dc9781ef019eb87c424ff04dc1d
Marius Bjoernstad <paalmbj@dev-lims.ous.nsc.local>
2015-09-30 15:31:27 +0000

    alpha version of pooling / normalisation

--- normalisation/poomalisation.py
+++ normalisation/poomalisation.py
@@ -4,10 +4,6 @@ import StringIO
 from genologics.lims import *
 from genologics import config
 
-def get_buffer_vol(normalised_concentration, input_volume, input_concentration):
-    return input_volume * (input_concentration * 1.0 / normalised_concentration - 1.0)
-
-
 def get_row_key(row):
     container = row[2]
     well = row[3]
@@ -23,15 +19,16 @@ def main(process_id, output_file_id):
 
     header = [
             "Pool",
+            "Dest. cont.",
+            "Well",
+            "Pool volume",
+            "Buffer volume",
             "Sample",
             "Source cont.",
             "Well",
-            "Dest. cont.",
-            "Well",
-            "Input molarity",
-            "Input volume",
-            "Normalised molarity",
-            "Buffer volume"
+            "Sample conc.",
+            "Sample volume",
+            "Normalised conc.",
             ]
 
 
@@ -43,51 +40,73 @@ def main(process_id, output_file_id):
     lims.get_batch(process.all_inputs(unique=True) + process.all_outputs(unique=True))
     lims.get_batch(input.samples[0] for input in process.all_inputs(unique=True))
 
-    update_outputs = set()
-    
+    try:
+        norm_conc = process.udf['Pool concentration (nM)']
+        pool_volume = process.udf['Pool volume']
+    except KeyError, e:
+        print str(e), "not specified"
+        sys.exit(1)
+
     rows = []
     for pool in step.pools.pooled_inputs:
-        output = pool.output
+        output = pool.output # output already fetched in batch, as process input
+        output.udf['Normalized conc. (nM)'] = norm_conc
+
+        target_sample_conc = norm_conc * 1.0 / len(pool.inputs)
+        target_sample_conc_str = "%4.2f" % target_sample_conc
+        sample_volumes = [pool_volume * target_sample_conc / input.udf['Molarity'] for input in pool.inputs]
+        buffer_volume = pool_volume - sum(sample_volumes)
+
+        if buffer_volume < 0:
+            print "Total sample volume in pool exceeds", pool_volume, "uL. Reduce the Pool volume and try "\
+                   "again."
+            sys.exit(1)
+
         dest_container = output.location[0].name
         dest_well = output.location[1]
-        for input in pool.inputs:
+
+        first_in_pool = True
+        for input, sample_volume in zip(pool.inputs, sample_volumes):
             sample_name = input.name.encode('utf-8')
-            try:
-                norm_conc = output.udf['Normalized conc. (nM)']
-            except KeyError:
-                norm_conc = process.udf['Default pool normalised concentration (nM)']
-                output.udf['Normalized conc. (nM)'] = norm_conc
-                update_outputs.add(output)
-            try:
-                input_vol = output.udf['Volume of input']
-            except KeyError:
-                input_vol = 1 # process.udf['Volume to take from inputs']
-                #output.udf['Volume of input'] = input_vol
-                #update_outputs.add(output)
-         
             input_mol_conc = input.udf['Molarity']
             input_mol_conc_str = "%4.2f" % (input.udf['Molarity'])
-            buffer_vol_str = "%4.2f" % (get_buffer_vol(norm_conc, input_vol, input_mol_conc))
+            sample_vol_str = "%4.2f" % sample_volume
             source_container = input.location[0].name
             source_well = input.location[1]
-            rows.append([
-                pool.name,
-                sample_name,
-                source_container,
-                source_well,
-                dest_container,
-                dest_well,
-                input_mol_conc_str,
-                input_vol,
-                norm_conc,
-                buffer_vol_str
-                ])
+            if first_in_pool:
+                rows.append([
+                    pool.name,
+                    dest_container,
+                    dest_well,
+                    "%4.2f" % pool_volume,
+                    "%4.2f" % buffer_volume,
+                    sample_name,
+                    source_container,
+                    source_well,
+                    input_mol_conc_str,
+                    sample_vol_str,
+                    target_sample_conc_str,
+                    ])
+                first_in_pool = False
+            else:
+                rows.append([
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    sample_name,
+                    source_container,
+                    source_well,
+                    input_mol_conc_str,
+                    sample_vol_str,
+                    target_sample_conc_str,
+                    ])
 
-    if update_outputs:
-        lims.put_batch(update_outputs)
 
-    output_file_name = output_file_id + "_normalisation.csv"
-    with open(output_file_name, 'wb') as out_file:
+    lims.put_batch(pool.output for pool in step.pools.pooled_inputs)
+
+    with open(output_file_id, 'wb') as out_file:
         out = csv.writer(out_file)
         out.writerow(header)
         out.writerows(rows)
