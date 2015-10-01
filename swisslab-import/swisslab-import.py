4c0acb544644b1062aed3e97833ee3104692cdd4
Marius Bjoernstad <paalmbj@dev-lims.ous.nsc.local>
2015-10-01 15:36:39 +0000

    new version of swisslab import with more validation

--- swisslab-import/swisslab-import.py
+++ swisslab-import/swisslab-import.py
@@ -14,11 +14,30 @@ from genologics import config
 #  PROCESS-ID: LIMS-ID of a process on which this script is run.
 #  SWL-FILE:   LIMS-ID of Artifact object for Excel file
 
+GENERAL_COLUMNS = {
+        "Test SWL Diag": str,
+        "Referral reason Diag": str,
+        "Archive position Diag": str,
+        "Sample conc. (ng/ul)": float,
+        "Analysis registered Diag": str,
+        "Gene panel Diag": str,
+        "Kit version Diag": str,
+        "Analysis type Diag": str
+        }
+
+TRIO_COLUMNS = {
+        "Family number Diag": str,
+        "Relation Diag": str,
+        "Sex Diag": str
+        }
+
+ALL_COLUMNS = frozenset(GENERAL_COLUMNS.keys() + TRIO_COLUMNS.keys())
+
 def get_swl_data(filename):
     """Parsing of sample information table in Excel format.
     
     Returns a dict indexed by sample name, where each value is a list 
-    of (udfname, udfvalue) pairs.
+    of (udfname, udfvalue) tuples.
     { sample name => [ (udfname, udfvalue), ... ] }
     """
     try:
@@ -28,30 +47,56 @@ def get_swl_data(filename):
         sys.exit(1)
 
     ws = wb['Samples']
-    udfnames = []
 
     assert ws['A1'].value == "Sample/Name"
+    # Get { header name => column index }
+    headers = {}
     for i in itertools.count(2):
         h = ws.cell(column=i, row=1).value
         if h:
-            udfnames.append(h)
+            if h in ALL_COLUMNS:
+                headers[h] = i
         else:
             break
 
-    done = False
+    missing = ALL_COLUMNS - frozenset(headers.keys())
+    if missing:
+        print "Missing the following columns in the Excel file:", ",".join(missing)
+        sys.exit(1)
+
     data = {}
     for row in itertools.count(2):
         sample_name = ws.cell(column=1, row=row).value
         if sample_name:
-            sample_values = []
-            for i, udfname in enumerate(udfnames):
-                v = ws.cell(column=i+2, row=row).value
-                sample_values.append((udfname, v))
-            data[sample_name] = sample_values
+            sample_data = get_all_fields(sample_name, ws, row, headers, GENERAL_COLUMNS, True)
+            trio_fields_required = any(
+                    val == "trio" and name == "Analysis type Diag"
+                    for name, val in sample_data
+                    )
+            sample_data += get_all_fields(sample_name, ws, row, headers, TRIO_COLUMNS, trio_fields_required)
+            data[sample_name] = sample_data
         else:
             break
 
     return data
+
+
+def get_all_fields(sample_name, ws, row, headers, columns, required):
+    """Get a list of tuples, one for each column in the list columns"""
+
+    result = []
+    for col, value_type in columns.items():
+        v = ws.cell(column=headers[col], row=row).value
+        if not v is None:
+            try:
+                result.append((col, value_type(v)))
+            except ValueError:
+                print "Cannot convert '" +  str(v) + "' to a", value_type, "for sample", sample_name, ", column", col
+                sys.exit(1)
+        elif required:
+            print "Missing required value for '", col, "' for sample", sample_name
+            sys.exit(1)
+    return result
             
 
 def main(process_id, swl_file_id):
@@ -79,8 +124,7 @@ def main(process_id, swl_file_id):
             sys.exit(1)
 
         for udfname, udfval in fields:
-            print "on", sample.name, "setting", udfname, "to", udfval
-            sample.udf[udfname] = str(udfval)
+            sample.udf[udfname] = udfval
 
     lims.put_batch(samples)
     print "Imported data for", len(samples), "samples"
