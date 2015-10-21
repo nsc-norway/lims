a1043677f5a73aa765d9cb0e780c8971695bbd62
fa2k <pmb@fa2k.net>
2015-10-21 09:51:57 +0200

    moving recently completed tracking from 99_processed.py in pipeline

---
+++ sequencing/complete-sequencing.py
@@ -0,0 +1,28 @@
+# Move run into processed/ on primary storage, and mark as completed
+# in LIMS 
+
+import sys
+from datetime import date
+from genologics.lims import *
+from genologics import config
+
+# Container UDFs to track flow cells
+RECENTLY_COMPLETED_UDF = "Recently completed"
+PROCESSED_DATE_UDF = "Processing completed date"
+
+def main(process_id):
+    lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
+    process = Process(lims, id=process_id)
+    
+    inputs = process.all_inputs(unique=True)
+    flowcells = set(i.location[0] for i in inputs)
+    if len(flowcells) == 1:
+        fc = next(iter(flowcells))
+        fc.get()
+        # Tracking UDF for "overview" page
+        fc.udf[RECENTLY_COMPLETED_UDF] = True
+        fc.udf[PROCESSED_DATE_UDF] = date.today()
+        fc.put()
+
+main(sys.argv[1])
+
