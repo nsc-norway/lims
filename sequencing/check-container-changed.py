2c97db636b5efebe07163907ac6a1599bf2e090a
Marius Bjoernstad <paalmbj@dev-lims.ous.nsc.local>
2015-10-21 13:54:06 +0000

    using the right protocol for analytes()

--- sequencing/check-container-changed.py
+++ sequencing/check-container-changed.py
@@ -8,7 +8,8 @@ from genologics import config
 def main(process_id):
     lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
     process = Process(lims, id=process_id)
-    flowcells = set(i.location[0] for i in process.analytes())
+    # analytes() returns tuples ('Output', [Analyte, ...]).
+    flowcells = set(ana.location[0] for ana in process.analytes()[0])
     if any(fc.name == fc.id for fc in flowcells):
         print "Please make sure container names are changed before continuing."
         sys.exit(1)
