a8dd899cb099a19567ebce01e16876941ba6ded0
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2015-12-03 15:03:03 +0100

    untested attempt to support two alternative column set outputs
    to be tested tomrorow

--- sample-prep/aliquote-volume.py
+++ sample-prep/aliquote-volume.py
@@ -6,6 +6,24 @@ from genologics import config
 
 DEFAULT_QUANTITY = 750
 
+
+WORKSHEET_HEADER = [
+        "Project",
+        "Sample",
+        "Conc. (ng/uL)",
+        "From well",
+        "To well",
+        "Volume to aliquote (uL)",
+        "DNA quantity (ng)"
+        ]
+
+ROBOT_HEADER = [
+        "SourceBC",
+        "SourceWell",
+        "DestinationWell",
+        "Volume"
+        ]
+
 def sort_key(elem):
     input, output, sample, qc = elem
     container, well = output.location
@@ -13,21 +31,20 @@ def sort_key(elem):
     return (container, int(col), row)
 
 
-def main(process_id, output_file_id):
+def main(process_id, mode):
+
     lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
     process = Process(lims, id=process_id)
 
-    out_buf = StringIO.StringIO()   
+    HEADER = {
+            "robot": ROBOT_HEADER,
+            "worksheet": WORKSHEET_HEADER
+            }
 
-    header = [
-            "Project",
-            "Sample",
-            "Conc. (ng/uL)",
-            "From well",
-            "To well",
-            "Volume to aliquote (uL)",
-            "DNA quantity (ng)"
-            ]
+    ROW = {
+            "robot": get_robot_lime,
+            "worksheet": get_worksheet_line
+            }
 
     inputs = []
     outputs = []
@@ -38,56 +55,73 @@ def main(process_id, output_file_id):
             inputs.append(input)
             outputs.append(output)
 
-    lims.get_batch(inputs + outputs)
-    samples = [input.samples[0] for input in inputs]
-    lims.get_batch(samples)
     qc_results = lims.get_qc_results(inputs, "Quant-iT QC Diag 1.0")
-    lims.get_batch(qc_results)
+    lims.get_batch(inputs + outputs + qc_results)
+    lims.get_batch(samples)
 
     updated_outputs = []
     warning = []
-    rows = []
     i_o_s_q = zip(inputs, outputs, samples, qc_results)
-    for input, output, sample, qc_result in sorted(i_o_s_q, key=sort_key):
-        project_name = input.samples[0].project.name.encode('utf-8')
-        sample_name = input.name.encode('utf-8')
-        dest_container = output.location[0].name
-        dest_well = output.location[1]
-
+    for input, output, qc_result in sorted(i_o_s_q, key=sort_key):
+        
+        # Update the UDF of the output to the default, if it's not set
         try:
-            norm_mass = output.udf['Amount of DNA per sample (ng)']
+            quantity = output.udf['Amount of DNA per sample (ng)']
         except KeyError:
-            norm_mass = DEFAULT_QUANTITY
-            output.udf['Amount of DNA per sample (ng)'] = norm_mass
+            quantity = DEFAULT_QUANTITY
+            output.udf['Amount of DNA per sample (ng)'] = quantity
             updated_outputs.append(output)
 
         input_conc = qc_result.udf['Concentration']
-        sample_volume = norm_mass * 1.0 / input_conc
+        volume = quantity * 1.0 / input_conc
 
-        source_container = input.location[0].name
-        source_well = input.location[1]
-
-        rows.append([
-            project_name,
-            sample_name,
-            "%4.2f" % input_conc,
-            source_well,
-            dest_well,
-            "%4.2f" % sample_volume,
-            "%4.2f" % norm_mass
-            ])
+        rows.append(ROW[mode](
+                input, output, qc_result,
+                quantity, volume
+               ))
 
     lims.put_batch(updated_outputs)
 
-    output_file_name = output_file_id + "_aliquote.csv"
+    output_file_name = output_file_id + "_" + mode + ".csv"
     with open(output_file_name, 'wb') as out_file:
         out = csv.writer(out_file)
-        out.writerow(header)
+        out.writerow(HEADER[mode])
         out.writerows(rows)
 
     if warning:
         print "Warning: too low input concentration for samples:", ", ".join(warning)
         sys.exit(1)
+    
 
-main(sys.argv[1], sys.argv[2])
+
+def get_robot_line(input, output, qc_result, quantity, volume):
+    return [
+            "Hybridisering",
+            input.location[1].replace(":",""),
+            output.location[1].replace(":", ""),
+            volume
+            ]
+
+def get_worksheet_line(input, output, qc_result, quantity, volume):
+    project_name = input.samples[0].project.name.encode('utf-8')
+    sample_name = input.name.encode('utf-8')
+    dest_container = output.location[0].name
+    dest_well = output.location[1]
+
+    source_container = input.location[0].name
+    source_well = input.location[1]
+
+    return [
+            project_name,
+            sample_name,
+            "%4.2f" % input_conc,
+            source_well,
+            dest_well,
+            "%4.2f" % volume,
+            "%4.2f" % quantity
+            ]
+
+
+
+main(*sys.argv[1:4])
 
