3179cd96a34d98007b765e46abc0b65680bff5c5
Marius Bjoernstad <paalmbj@dev-lims.ous.nsc.local>
2016-01-14 14:13:40 +0000

    Using for another case, and this actually works now

--- set-reagent-labels/modify-reagent-types.py
+++ set-reagent-labels/modify-reagent-types.py
@@ -9,27 +9,29 @@ from genologics import config
 def main():
     lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
 
-    cells = open("xt.txt").read().split()
-    sequence_position = dict(
-            (sequence, col)
-            for col, sequence
-            in zip(cells[::2], cells[1::2])
-            )
+    table = [l.split() for l in open('nextera_name_index.txt').readlines()]
+    db = dict(("%s (%s)" % (t[1], t[2]), t) for t in table)
 
     updated = []
     for reg in lims.get_reagent_types():
-        seq_match = re.match(r"SureSelect XT2 Index \d+ \(([ATCG]+)\)", reg.name)
-        if seq_match:
-            sequence = seq_match.group(1)
-            pos = sequence_position[sequence]
-            pos_sortable = pos[1:] + "-" + pos[0]
-            new_name = "SureSelect XT2 Index %s (%s)" % (pos_sortable, sequence)
+        old_name = reg.name
+        row = db.get(old_name)
+        if row:
+            new_name = "%s %s (%s)" % tuple(row)
             reg.name = new_name
             updated.append(reg)
+            del db[old_name]
 
-    for reg in updated:
-        print "Updating ", reg.name
-        reg.put()
+    if len(updated) == 96:
+        for reg in updated:
+            print "Updating ", reg.name
+            return
+            reg.put()
+    else:
+        print "Found unexpected number of indexes:", len(updated)
+        print "Missing:"
+        for x in db.keys():
+            print x
 
 main()
 
