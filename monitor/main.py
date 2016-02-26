178598012b5473a66b80de1ebb4be1a985fa9828
Marius Bjoernstad <paalmbj@dev-lims.ous.nsc.local>
2016-02-26 09:46:50 +0100

    correcting a kind of typo

--- monitor/main.py
+++ monitor/main.py
@@ -83,7 +83,7 @@ def get_sequencing_process(process):
     first_in_artifact = first_io[0]['uri']
 
     processes = process.lims.get_processes(inputartifactlimsid=first_in_artifact.id)
-    seq_processes = [proc for proc in processes if proc.type.name in [p[1] for p in nsc.SEQ_PROCESSES]]
+    seq_processes = [proc for proc in processes if proc.type.name in [p[1] for p in SEQ_PROCESSES]]
     # Use the last sequencing process. In case of crashed runs, this will be the right one.
     return seq_processes[-1]
 
