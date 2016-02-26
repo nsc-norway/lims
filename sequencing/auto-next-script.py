b6159c459c99ab3e4ce6d79f10214a8cbba93378
Marius Bjoernstad <paalmbj@dev-lims.ous.nsc.local>
2016-02-26 09:52:58 +0100

    Adding better selection of sequensing process here too

--- sequencing/auto-next-script.py
+++ sequencing/auto-next-script.py
@@ -39,10 +39,12 @@ def get_sequencing_process(process):
     first_in_artifact = first_io[0]['uri']
 
     processes = process.lims.get_processes(inputartifactlimsid=first_in_artifact.id)
-
-    for proc in processes:
-        if proc.type.name in [p[1] for p in SEQ_PROCESSES]:
-            return proc
+    seq_processes = [proc for proc in processes if proc.type.name in [p[1] for p in SEQ_PROCESSES]]
+    # Use the last sequencing process. In case of crashed runs, this will be the right one.
+    try:
+        return seq_processes[-1]
+    except IndexError:
+        return None
 
 
 def is_sequencing_finished(process):
