e3068885196e4fcef2a7e16526a9fa904c318bb1
Marius Bjoernstad <paalmbj@dev-lims.ous.nsc.local>
2016-03-21 13:17:21 +0100

    Removing an unused import

--- monitor/main.py
+++ monitor/main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, url_for, request, Response, redirect
 from genologics.lims import *
 from genologics import config
 import re
-import requests
 import datetime
 import threading
 from functools import partial
