57c4a3feea0c042b3a58824e54110263d9f53101
Marius Bjoernstad <paalmbj@dev-lims.ous.nsc.local>
2016-03-30 17:13:00 +0200

    initial work on the base counter

--- base-counter/server.py
+++ base-counter/server.py
@@ -39,15 +39,54 @@ class RunBaseCounter(object):
 
     def get_cycle(self):
         for cycle in range(self.current_cycle, self.total_cycles):
-            test_path = os.path.join(run_dir, "Data", "Intensities", "BaseCalls", "L001", "C{0}.1" % (cycle+1))
-            if not os.path.exists(test_path):
+            test_paths = [
+                    os.path.join(
+                        self.run_dir, "Data", "Intensities", "BaseCalls", "L001",
+                        "C{0}.1".format(cycle+1)
+                        ),
+                    os.path.join(
+                        self.run_dir, "Data", "Intensities", "BaseCalls", "L001",
+                        "{0:04d}.bcl.bgzf".format(cycle+1)
+                        )
+                    ]
+            if not any(os.path.exists(test_path) for test_path in test_paths):
                 return cycle
         return self.total_cycles
+
+    def update_clusters_pf(ds, process, current_cycle):
+        try:
+            all_df = ds.TileMetrics().df
+        except ValueError:
+            return # No information yet
+        df = all_df[all_df.code == 103] # Number of clusters PF
+        r1cycles = process.udf['Read 1 Cycles']
+        reads = 1
+        if current_cycle > r1cycles:
+            reads = 2
+
+        lanes = process.all_inputs(resolve=True)
+        for lane_ana in lanes:
+            lane_str = lane_ana.location[1].split(":")[0]
+            if lane_str == "A":
+                lane = 1
+            else:
+                lane = int(lane_str)
+            clusters = df[df.lane == lane].value.sum()
+            for i_read in range(1, reads+1):
+                lane_ana.udf['Clusters PF R%d' % i_read] = clusters
+        lims.put_batch(lanes)
 
     def update(self):
         if not self.read_config:
             self.set_metadata()
         self.current_cycle = self.get_cycle()
+        self.clusters = self.get_clusters()
+        self.booked = self.current_cycle * self.clusters
+        self.last_update = time.time()
+
+    @property
+    def projected_rate(self):
+        pass
 
     @property
     def basecount(self):
@@ -62,8 +101,14 @@ def get_main():
 def event(rbc):
     i=0
     while True:
+        rbc.update()
         event = "event: run.%s\n" % (rbc.run_id)
-        event += 'data: {"basecount": %d}\n\n' % (rbc.basecount)
+        print "bc", rbc.basecount
+        print "rate", rbc.rate
+        event += 'data: {"basecount": %d, "rate": %d}\n\n' % (
+                rbc.basecount,
+                rbc.rate
+                )
         yield event
         time.sleep(2)
 
--- base-counter/static/status.js
+++ base-counter/static/status.js
@@ -1,35 +1,42 @@
 
 var eventSource = new EventSource("/status");
-var updateHandlers = []
+var refreshHandlers = []
 
 function runBaseCounter(span, run_id) {
 	var bc = {
 		'run_id': run_id,
 		'span': span,
 		'update_time': 0,
-		'finished': false,
-		'data': {},
+		'basecount': 0,
+		'data': {'basecount': 0, 'rate': 0},
 		'update': function(e) {
 			this.data = JSON.parse(e.data);
 		},
-		'refresh': function() {
+		'refresh': function(time) {
+			// Update handler
+			var target = (this.data.rate * time - this.update_time) + this.data.basecount;
+			this.basecount = target;
 			span.innerHTML = this.basecount;
-			return this.data.finished;
+			return !this.data.finished;
 		}
 	};
 	eventSource.addEventListener("run." + run_id, bc.update);
-	updateHandlers.push(bc.refresh);
+	bc.refresh(0);
+	refreshHandlers.push(bc);
 }
 
 $().ready(function() {
 	var span = document.getElementById("base-count");
 	r = runBaseCounter(span, "160329_M01132_0133_000000000-AMY9J");
+	//refresher();
+	updateCount();
 });
 
 function updateCount() {
-	for (var i = updateHandlers.length; i >= 0; i--) {
-		if (!updateHandlers[i]()) {
-			updateHandlers.pop();
+	var time = new Date().getTime();
+	for (var i = refreshHandlers.length-1; i >= 0; i--) {
+		if (!refreshHandlers[i].refresh(time)) {
+			refreshHandlers.pop();
 		}
 	}
 }
