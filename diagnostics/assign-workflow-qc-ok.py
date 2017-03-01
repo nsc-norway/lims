db1dc1bd1fab80391e02e7fe40da67e894c9692e
Marius Bjoernstad <paalmbj@dev-lims.sequencing.uio.no>
2017-03-01 16:19:30 +0100

    Re-structured workflow assignment script, no significant changes

--- diagnostics/assign-workflow-qc-ok.py
+++ diagnostics/assign-workflow-qc-ok.py
@@ -14,13 +14,17 @@ def main(process_id, workflow_name):
     except IndexError:
         print "Unknown workflow '", workflow_name, "'"
         sys.exit(1)
-    qc_ok_inputs = (ana for ana in process.all_inputs(unique=True, resolve=True) if ana.qc_flag=="PASSED")
-    samples = [s for ana in qc_ok_inputs for s in ana.samples]
-    lims.get_batch(samples)
-    diag_samples = [s for s in samples if s.project and s.project.udf.get('Project type') == "Diagnostics"]
-    root_analytes = (s.artifact for s in diag_samples)
+    lims.get_batch(process.all_inputs(unique=True) + process.all_outputs(unique=True))
+    lims.get_batch(output.samples[0] for output in process.all_outputs(unique=True))
+    routables = []
+    for i, o in process.input_output_maps:
+        if o['output-generation-type'] == "PerReagentLabel":
+            if i['uri'].qc_flag == "PASSED":
+                if o['uri'].samples[0].project.udf.get('Project type') == "Diagnostics":
+                    routables.append(o['uri'].samples[0].artifact)
 
-    lims.route_analytes(root_analytes, workflow)
+    if routables:
+        lims.route_analytes(routables, workflow)
 
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2])
