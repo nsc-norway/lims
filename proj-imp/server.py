8de5a6336e8b367c277d84a1c67cef05435db28b
Marius Bjoernstad <paalmbj@dev-lims.sequencing.uio.no>
2017-05-29 17:25:39 +0200

    More initial development on project importer

--- proj-imp/server.py
+++ proj-imp/server.py
@@ -8,7 +8,14 @@ from flask import Flask, url_for, abort, jsonify, Response, request,\
         render_template, redirect
 from werkzeug.utils import secure_filename
 
+from urlparse import urljoin
+import requests
+
+from genologics.lims import *
+from genologics import config
+
 app = Flask(__name__)
+lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
 
 # Global dict mapping type name to job object for project type
 # (only one project may be created at a time)
@@ -43,6 +50,7 @@ def get_project_def(project_type):
 def get_project_start_page(project_type):
     return render_template("index.html", **get_project_def(project_type))
 
+
 @app.route("/<project_type>/submit", methods=["POST"])
 def submit_project(project_type):
     """Trigger creation of a project. Instantly returns a
@@ -73,32 +81,100 @@ def submit_project(project_type):
     with jobs_lock:
         job = jobs.setdefault(project_type, Job(project_type))
         if not job.active:
-            job.start(username, password, project_data['project_title'], file_name, file_data.getvalue())
-    return redirect(url_for('get_projet_status', project_type=project_type))
+            try:
+                job.start(username, password, project_data['project_title'], file_name, file_data.getvalue())
+            except LimsCredentialsError:
+                # Why abort() here, not render_template?: We can't put the file back into the response,
+                # so better encourage the user to press back and try again (with the file).
+                abort(403, "Incorrect username or password, please go back and try again.")
+
+    return redirect(url_for('get_project_status', project_type=project_type))
+
 
 @app.route("/<project_type>/status")
 def get_project_status(project_type):
-    return render_template("status.html", **get_project_data(project_type))
+    """Status page"""
+    return render_template("status.html", **get_project_def(project_type))
+
 
 @app.route("/<project_type>/stream")
 def get_status():
-    """Status page"""
+    """SSE stream with progress."""
     pass
+
+class LimsCredentialsError(ValueError):
+    pass
+
+class Task(object):
+    def __init__(self, name, function):
+        self.name = name
+        self.function = function
+        self.running = False
+        self.completed = False
+        self.error = False
+        self.error_message = None
+
+    def __call__(self):
+        try:
+            self.running = True
+            self.function()
+        except Exception as e:
+            self.running = False
+            self.error = True
+            self.error_message = e.message
+            return False
+        else:
+            self.completed = True
+            self.running = False
+            return True
 
 class Job(object):
     def __init__(self, project_type):
         self.project_type = project_type
+        self.project_title = None
+        self.sample_filename = None
+        self.sample_file_data = None
+        self.active = False
+        self.state = {}
+        self.tasks = [
+                Task("Create project", self.create_project)
+                ]
 
     def start(self, username, password, project_title, sample_filename, sample_file_data):
-        pass
+        """Start an import task with the specified parameters.
+        
+        This function is not thread safe! Syncrhonization must be handled by the caller.
+        """
+        assert not self.active
+
+        uri = urljoin(config.BASEURI, 'api')
+        r = requests.get(uri, auth=(username, password))
+        if r.status_code not in [403, 200]:
+            # Note: 403 is OK! It indicates that we have a valid password, but
+            # are a Researcher user and thus not allowed to access the API. 
+            # If the password is wrong, we will get a 401.
+            raise LimsCredentialsError()
+
+        self.project_title = project_title
+        self.sample_filename = sample_filename
+        self.sample_file_data = sample_file_data
+
+        thread = threading.Thread(target=self.run)
+        self.active = True
+        thread.start()
 
     def run(self):
+        try:
+            for task in self.tasks:
+                if not task():
+                    break
+        finally:
+            self.active = False
+
+    def create_project(self):
         pass
 
-    @property
-    def active(self):
-        return False
-    
+
 
 # Start deveopment server if called on the command line
 if __name__ == "__main__":
