31c7b8b35dc8804b964d975a97e18e7d3b0bfb40
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2017-06-19 16:22:07 +0200

    Skeleton scripts for lot tracker

---
+++ lot-tracker/lot-show-reactions.py
@@ -0,0 +1,14 @@
+import sys
+from genologics.lims import *
+from genologics import config
+
+def main(process_id):
+    """AAA"""
+    lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD) 
+    step = Step(lims, id=process_id)
+    if step.reagentlots:
+        for lot in step.reagentlots.reagent_lots:
+            pass
+
+if __name__ == "__main__":
+    main(*sys.argv)
