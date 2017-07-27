0d8d5476ff040e0582355a72029c313df2456a47
Marius Bjoernstad <paalmbj@dev-lims.sequencing.uio.no>
2017-07-27 17:14:51 +0200

    New hamilton file format

--- snp-id/hamilton_snpid.py
+++ snp-id/hamilton_snpid.py
@@ -17,12 +17,6 @@ process = Process(lims, id=sys.argv[1])
 wb = xlwt.Workbook()
 ws = wb.add_sheet('Sheet1')
 
-def sort_key(elem):
-    input, output = elem
-    container, well = output.location
-    row, col = well.split(":")
-    return (container, int(col), row)
-
 headers = [
     "Sample_Number",
     "Labware",
@@ -35,25 +29,35 @@ for i, header in enumerate(headers):
     ws.write(0, i, header)
 
 i_os = process.input_output_maps
-inputs_outputs = [(i['uri'], o['uri']) for i,o in i_os if o['output-generation-type'] == 'PerInput']
-lims.get_batch([i for i,o in inputs_outputs] + [o for i,o in inputs_outputs])
-lims.get_batch(i.samples[0] for i, o in inputs_outputs)
-for row_index, (input, output) in enumerate(sorted(inputs_outputs, key=sort_key)):
-    ws.write(row_index+1, 0, input.name)
-    ws.write(row_index+1, 1, "Rack{}".format(1 + (row_index + 8) // 32))
-    ws.write(row_index+1, 2, 1 + (row_index + 8) % 32 )
-    try:
-        conc = input.samples[0].udf['Sample conc. (ng/ul)']
-    except KeyError:
-        ws.write(row_index+1, 3, "MISSING")
-        continue
-    if conc >= 9 and conc <= 180:
-        ws.write(row_index+1, 3, 2)
-    else:
-        # Compute 3 ng/uL in 45 uL total volume
-        sample_vol = (3 * 45) / conc
-        ws.write(row_index+1, 3, sample_vol)
-    ws.write(row_index+1, 4, output.location[1].replace(":", ""))
+location_io = dict(
+        (o['uri'].location[1], ((i['uri'], o['uri'])))
+        for i,o in i_os if o['output-generation-type'] == 'PerInput'
+        )
+lims.get_batch([i for i,o in location_io.values()] + [o for i,o in location_io.values()])
+lims.get_batch(i.samples[0] for i, o in location_io.values())
+for row_index in range(96):
+    well = "ABCDEFGH"[row_index % 8] + ":" + str((row_index // 8) + 1)
+    value = location_io.get(well)
+    if value:
+        input, output = value
+        ws.write(row_index+1, 0, input.name)
+        ws.write(row_index+1, 1, "Rack{}".format(1 + (row_index + 8) // 32))
+        ws.write(row_index+1, 2, 1 + (row_index + 8) % 32 )
+        try:
+            conc = input.samples[0].udf['Sample conc. (ng/ul)']
+        except KeyError:
+            ws.write(row_index+1, 3, "MISSING")
+            continue
+        if conc >= 9 and conc <= 180:
+            ws.write(row_index+1, 3, 2)
+        elif conc < 1:
+            ws.write(row_index+1, 3, 1)
+        else:
+            # Compute 3 ng/uL in 45 uL total volume
+            sample_vol = (3 * 45) / conc
+            ws.write(row_index+1, 3, sample_vol)
+
+    ws.write(row_index+1, 4, well.replace(":", ""))
 
 wb.save(sys.argv[2] + '-hamilton.xls')
 
