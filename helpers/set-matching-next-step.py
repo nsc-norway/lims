8b679969fb00b8455673a3d3096cf0681a52bdf0
P}l Marius Bj|rnstad <paalmbj@sandbox-lims.sequencing.uio.no>
2017-08-18 16:33:00 +0200

    Alternative next step script, not to be used

---
+++ helpers/set-matching-next-step.py
@@ -0,0 +1,33 @@
+import sys
+import re
+from genologics import config
+from genologics.lims import *
+
+# Script to fill the 'next action' field with the first next step 
+# matching the specified regular expression
+
+# NOTE: This script was never used or tested! Left here in case we
+# need it later, but may not work...
+
+def main(process_id, name_pattern):
+    lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
+    step = Step(lims, id=process_id)
+    if step.configuration.transitions:
+        for tn in step.configuration.transitions:
+            if re.match(name_pattern, tn['name']):
+                next_step_uri = tn.get("next-step-uri")
+                action = "nextstep"
+                break
+    else:
+        action = "complete"
+
+    for next_action in step.actions.next_actions:
+        if next_action.get('action') != "remove": # Don't set next action for controls, which have default "remove"
+            if action == "nextstep":
+                next_action['step-uri'] = next_step_uri
+            next_action['action'] = action
+
+main(
+        process_id=sys.argv[1],
+        name_pattern=sys.argv[2]
+        )
