14ee11f94e084deec9645aff6a6e1584dfce8804
Marius Bjoernstad <paalmbj@dev-lims.sequencing.uio.no>
2017-10-11 15:33:28 +0200

    Finished step runnder script

--- helpers/workflow-run-last-step.py
+++ helpers/workflow-run-last-step.py
@@ -1,5 +1,6 @@
-# This script takes a list of queue-IDs on the command line. For each queue, it 
-# takes all artifacts into a step, and pushes them through a step. 
+# This script takes a pattern to match a workflow name on the command line. 
+# It finds the queue of the last stage of the workflow, takes all artifacts in that
+# queue into a step, and pushes them through the step. 
 
 # It is intended to run in a cron job
 
@@ -8,30 +9,34 @@
 # It will work with steps with automatic triggers
 
 import sys
+import re
 import time
 from genologics.lims import *
 from genologics import config
 
-def main(qids):
+def main(workflow_pattern):
     lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
     try:
         lims.check_version()
     except:
         return # If LIMS not reachable then die
-    for qid in qids:
-        q = Queue(lims, id=qid)
-        if q.artifacts:
-            step = lims.create_step(q.protocol_step_config, q.artifacts)
-            while step.current_state.upper() != "COMPLETED":
-                if step.current_state == "Assign Next Steps":
-                    lims.set_default_next_step(step)
-                if not step.program_status or step.program_status.status not in ["QUEUED", "RUNNING"]:
-                    step.advance()
-                time.sleep(0.5)
-                step.get(force=True)
-                if step.program_status:
-                    step.program_status.get(force=True)
+    workflow_data = lims.get_workflows(add_info=True)
+    for workflow, info in zip(*workflow_data):
+        if info['status'] == "ACTIVE" and re.match(workflow_pattern, info['name']):
+            q = workflow.stages[-1].step.queue()
+            if q.artifacts:
+                step = lims.create_step(q.protocol_step_config, q.artifacts)
+                while step.current_state.upper() != "COMPLETED":
+                    if step.current_state == "Assign Next Steps":
+                        lims.set_default_next_step(step)
+                    if not step.program_status or step.program_status.status not in ["QUEUED", "RUNNING"]:
+                        step.advance()
+                    time.sleep(0.5)
+                    step.get(force=True)
+                    if step.program_status:
+                        step.program_status.get(force=True)
+            break
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main(workflow_pattern=sys.argv[1])
 
