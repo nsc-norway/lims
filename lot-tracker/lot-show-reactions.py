c99afd5f45f2664301352485ab52f1713b172965
Marius Bjoernstad <paalmbj@dev-lims.sequencing.uio.no>
2017-11-07 17:38:05 +0100

    Initial working lot tracker version

--- lot-tracker/lot-show-reactions.py
+++ lot-tracker/lot-show-reactions.py
@@ -1,14 +1,60 @@
 import sys
+import re
 from genologics.lims import *
 from genologics import config
 
 def main(process_id):
-    """AAA"""
+    """Show the remaining reactions in the selected lots. This works with steps which have only one kit
+    type configured.
+    """
     lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD) 
     step = Step(lims, id=process_id)
-    if step.reagentlots:
-        for lot in step.reagentlots.reagent_lots:
-            pass
+    process = Process(lims, id=process_id)
+    reactions_needed = len(process.all_inputs(unique=True))
+    if not step.reagentlots:
+        sys.exit(0)
+
+    # Get the name of the kit type
+    lots = step.reagentlots.reagent_lots
+    kit_names = set(lot.reagent_kit_name for lot in lots)
+    if len(kit_names) != 1:
+        print("This script only supports steps with exactly one configured kit type")
+        sys.exit(1)
+    kit_name = next(iter(kit_names))
+
+    # Get the total number of reactions in each box of this kit type
+    rxn_match = re.search(r"\b(\d+)rxn\b", kit_name)
+    if not rxn_match:
+        print("The kit name does not include the number of reactions (Nrxn).")
+        sys.exit(1)
+    kit_type_num_reactions = int(rxn_match.group(1))
+
+    # Constructing one line for each lot, with textual information
+    lot_data_lines = []
+    for lot in lots:
+        # For new lots, we only have this information
+        lot_remaining_reactions = kit_type_num_reactions
+        # Try to get remaining reactions from notes
+        if lot.notes:
+            reactions_match = re.match(r"\d+", lot.notes)
+            if reactions_match:
+                lot_remaining_reactions = int(reactions_match.group(0))
+
+        use_reactions = min(lot_remaining_reactions, reactions_needed)
+        reactions_needed -= use_reactions
+        output_text = "Name: {0}, Lot#: {1}, Remaining: {2}, Use: {3}".format(
+                        lot.name,
+                        lot.lot_number,
+                        lot_remaining_reactions,
+                        use_reactions
+                        )
+        if use_reactions == 0:
+            output_text += ", Lot not used"
+        lot_data_lines.append(output_text)
+
+    process.udf['Lots used'] = "\n".join(lot_data_lines)
+    process.put()
+
 
 if __name__ == "__main__":
-    main(*sys.argv)
+    main(process_id=sys.argv[1])
--- lot-tracker/lot-use-reactions.py
+++ lot-tracker/lot-use-reactions.py
@@ -1,15 +1,71 @@
 import sys
+import re
 from genologics.lims import *
 from genologics import config
 
+# NOTE! This script needs to be run with Python 3 in order to correctly
+# match nbsp characters in the regex.
+
 def main(process_id):
-    """AAA"""
+    """Script to register lot usage and disable expired lots."""
+
     lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD) 
+    process = Process(lims, id=process_id)
     step = Step(lims, id=process_id)
-    if step.reagentlots:
-        for lot in step.reagentlots.reagent_lots:
-            pass
+    
+    # Get the name of the kit type (same as in the "show lots")
+    lots = step.reagentlots.reagent_lots
+    kit_names = set(lot.reagent_kit_name for lot in lots)
+    if len(kit_names) != 1:
+        print("This script only supports steps with exactly one configured kit type")
+        sys.exit(1)
+    kit_name = next(iter(kit_names))
+    # Get the total number of reactions in each box of this kit type (copied from "show lots" script)
+    rxn_match = re.search(r"\b(\d+)rxn\b", kit_name)
+    if not rxn_match:
+        print("The kit name does not include the number of reactions (Nrxn).")
+        sys.exit(1)
+    kit_type_num_reactions = int(rxn_match.group(1))
+
+    try:
+        lots_used = process.udf['Lots used']
+    except KeyError:
+        print("Missing 'Lots used' field, this is a configuration error.")
+        sys.exit(1)
+    match_string = r"Name:\s*([^,]+),\s*Lot#:\s*([^,]+),\s*Remaining:\s*(\d+),\s*Use:\s*(\d+)"
+    for line in process.udf['Lots used'].splitlines():
+        match = re.match(match_string, line)
+        if match:
+            name, lotnum, remaining, used = match.groups()
+            lots = lims.get_reagent_lots(name=name, number=lotnum, kitname=kit_name)
+            if len(lots) == 0:
+                print("Lot number", lotnum, ", lot name", name, "not found.")
+                sys.exit(1)
+            elif len(lots) > 1:
+                print("Lot number", lotnum, ", lot name", name, "matches multiple lots, which should not be possible.")
+                sys.exit(1)
+            lot = lots[0]
+            before_remaining_reactions = None
+            # Get reactions from the lot notes again (to limit race conditions if multiple steps are open)
+            if lot.notes:
+                reactions_match = re.match(r"\d+", lot.notes)
+                if reactions_match:
+                    before_remaining_reactions = int(reactions_match.group(0))
+            # If there is nothing to be found in notes, we'll use the default number. Temporarily set the previous number
+            # of remaining reactions, we'll update it soon
+            if not before_remaining_reactions:
+                before_remaining_reactions = kit_type_num_reactions
+                prev_notes_text = lot.notes
+                lot.notes = "{0} reactions remaining.".format(before_remaining_reactions)
+                if prev_notes_text:
+                    lot.notes += " " + prev_notes_text
+
+            new_remaining_reactions = before_remaining_reactions - int(used)
+            lot.notes = re.sub(r"\d+", str(new_remaining_reactions), lot.notes)
+            if new_remaining_reactions == 0:
+                lot.state = "ARCHIVED"
+            lot.put()
 
 
 if __name__ == "__main__":
-    main(*sys.argv)
+    main(process_id=sys.argv[1])
