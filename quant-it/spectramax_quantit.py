4d69d5b60393314c44992a2b66b6776a1563ecc6
Marius Bjoernstad <paalmbj@dev-lims.sequencing.uio.no>
2017-12-01 16:47:30 +0100

    Changed the algorithm, doesn't work yet

--- quant-it/spectramax_quantit.py
+++ quant-it/spectramax_quantit.py
@@ -3,7 +3,7 @@ import sys
 from genologics import config
 from genologics.lims import *
 
-from scipy import stats
+import numpy
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pylab as plt
@@ -72,20 +72,40 @@ def main(process_id, graph_file_id, sample_volume, input_file_ids):
             print "Result file has invalid format (" + str(e) + ")."
             sys.exit(1)
 
+    # Concentrations (x)
+    scaled_concs = numpy.array([sv * STANDARD_VOLUME / sample_volume for sv in standards_concs])
+
+    # Counts (y)
     first_container = output_containers[0]
     standards_values = [container_data[first_container]["{0}:{1}".format(row, 1)] for row in ROWS]
-    scaled_concs = [sv * STANDARD_VOLUME / sample_volume for sv in standards_concs]
-    slope, intercept, r_value, p_value, std_err = stats.linregress(scaled_concs, standards_values)
-    process.udf['R^2'] = r_value**2
+    std0_value = scaled_concs[0]
+    process.udf['Std0 value'] = std0_value
+    shifted_values = numpy.array([c - std0_value for c in scaled_concs])
+    
+    stdcurve_fn = lambda x, a: a*x # y=ax
+    xdata = numpy.matrix([scaled_concs]).T
+    ydata = numpy.array(shifted_values)
+
+    slopes, _, _, _ = numpy.linalg.lstsq(xdata, ydata)
+    slope = slopes[0]
+
+    # Calculate R^2: we need the "total" sum of squares rel to mean, and residuals
+    mean = numpy.mean(shifted_values)
+    totalsum2 = ((shifted_values - mean)**2).sum()
+    residualsum2 = ((shifted_values - slope*scaled_concs)**2).sum()
+
+    process.udf['R^2'] = 1 - residualsum2 / totalsum2
 
     for o in result_files:
         if o.location[0] == first_container and o.location[1].endswith(":1"):
             print "Detected a sample in the position", o.location[1], "which should be a standard."
             sys.exit(1)
-        conc = (container_data[o.location[0]][o.location[1]] - intercept) / slope
+        conc = (container_data[o.location[0]][o.location[1]]) / slope
         o.udf['Concentration'] = conc
 
-    make_plot(sample_volume, scaled_concs, standards_values, intercept, slope, graph_file_id)
+    # Plot the data and the curve
+    # Intercept parameter is now fixed at 0
+    make_plot(sample_volume, scaled_concs, standards_values, 0, slope, graph_file_id)
     lims.put_batch(result_files)
 
     process.put()
