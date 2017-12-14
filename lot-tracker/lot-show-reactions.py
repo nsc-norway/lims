6e412479251db230a9d5a416f517a2886842a376
Marius Bjoernstad <paalmbj@dev-lims.sequencing.uio.no>
2017-12-14 14:46:02 +0100

    Lot tracker maybe more complete now, using lot name to store number of reactions

--- lot-tracker/lot-show-reactions.py
+++ lot-tracker/lot-show-reactions.py
@@ -1,15 +1,26 @@
 import sys
 import re
+from collections import defaultdict
 from genologics.lims import *
 from genologics import config
 
-def get_suggested_reactions_used_per_lot(kit_lots, num_samples):
-    exp_remain_lot = sorted((lot.expiry_date, get_remaining_reactions(lot), lot) for lot in this_kit_lots)
+
+def get_remaining_reactions(lot):
+    rm = re.search(r"-(\d+)R$", lot.name)
+    if rm:
+        return int(rm.group(1))
+    else:
+        raise ValueError("Unknown number of reactions for lot {0} / {1}.".format(lot.name, lot.lot_number))
+
+
+def get_suggested_reactions_used_per_lot(kit_lots, num_reactions_to_use):
+    exp_remain_lot = sorted((lot.expiry_date, get_remaining_reactions(lot), lot) for lot in kit_lots)
     num_reactions_left_to_use = num_reactions_to_use
     lot_used = []
-    for expiry, num_remain, lot in remain_lot:
+    for expiry, num_remain, lot in exp_remain_lot:
         use_of_this_lot = min(num_reactions_left_to_use, num_remain)
         lot_used.append((lot, use_of_this_lot))
+        num_reactions_left_to_use -= use_of_this_lot
     return lot_used
 
 
@@ -34,8 +45,10 @@ def main(process_id):
 
     for kit, lots in kits_lots.items():
         lot_data_lines.append("-- Kit: {0} --".format(kit.name))
-        for lot, usable in get_suggested_reactions_used_per_lot(lots, num_reactions_to_use):
-            pass # TODO!
+        for lot, suggested in get_suggested_reactions_used_per_lot(lots, num_reactions_to_use):
+            lot_data_lines.append("{0:15s} | {1:15s} | exp: {2} | use: {3}".format(
+                lot.name, lot.lot_number, lot.expiry_date, suggested
+                ))
 
     process.udf['Lots used'] = "\n".join(lot_data_lines)
     process.put()
