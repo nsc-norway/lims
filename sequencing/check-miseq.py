948c1cffd97fbc12e213fb1a2e1b94c50acb1b97
Marius Bjoernstad <paalmbj@dev-lims.sequencing.uio.no>
2018-02-22 14:49:02 +0100

    python version; added comments

--- sequencing/check-miseq.py
+++ sequencing/check-miseq.py
@@ -1,5 +1,5 @@
-# Check that container name is not the default (= LIMSID)
-
+# Check for multiple flow cells
+# This is a Python 3 script
 import sys
 from genologics.lims import *
 from genologics import config
@@ -9,10 +9,9 @@ def main(process_id):
     lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
     process = Process(lims, id=process_id)
     # analytes() returns tuples ('Output', [Analyte, ...]).
-    # Not using a batch for the container list because it will almost always be one
-    flowcells = set(lims.get_batch(ana.location[0] for ana in lims.get_batch(process.analytes()[0])))
+    flowcells = set(ana.location[0] for ana in lims.get_batch(process.analytes()[0]))
     if len(flowcells) > 1:
-        print "There should only be one flow cell in each MiSeq Run step, but {0} flow cells found.".format(len(flowcells))
+        print("There should only be one flow cell in each MiSeq Run step, but {0} flow cells found.".format(len(flowcells)))
         sys.exit(1)
 
 main(sys.argv[1])
