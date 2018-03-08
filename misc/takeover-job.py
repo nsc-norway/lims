2056e40a4342d79217375113bae34d815f2a8822
Marius Bjoernstad <paalmbj@dev-lims.sequencing.uio.no>
2018-03-08 15:36:29 +0100

    Added a step takeover job, for escalations

---
+++ misc/takeover-job.py
@@ -0,0 +1,25 @@
+# Changes the "technician" field of recently run processes which have "escalation"
+# The technician is set to API user, so the original user can complete the review
+# if required
+from genologics.lims import *
+from genologics import config
+from datetime import datetime, timedelta
+import sys
+
+lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
+
+try:
+    lims.check_version()
+except:
+    sys.exit(0) # Since this will run as a cron job, we just die silently if LIMS is unreachable
+
+two_hours_ago = (datetime.utcnow() - timedelta(hours=2)).strftime("%Y-%m-%dT%H:%M:%S+00:00")
+processes = lims.get_processes(last_modified=two_hours_ago)
+takeover_user = Researcher(lims, id="3")
+for process in processes:
+    if process.technician != takeover_user:
+        escalation = Step(lims, id=process.id).actions.escalation
+        if escalation and escalation['author'] == process.technician:
+            process.technician = takeover_user
+            process.put()
+
