388af80a73d0c280284a25b43765828bdea1a0e2
Marius Bjoernstad <paalmbj@dev-lims.sequencing.uio.no>
2018-04-25 15:14:02 +0200

    Added more checks

--- sequencing/check-container-fcid.py
+++ sequencing/check-container-fcid.py
@@ -8,7 +8,6 @@ import re
 def main(process_id):
     lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
     process = Process(lims, id=process_id)
-    # analytes() returns tuples ('Output', [Analyte, ...]).
     # Not using a batch for the container list because it will almost always be one
     flowcells = set(lims.get_batch(ana.location[0] for ana in lims.get_batch(process.analytes()[0])))
     if len(flowcells) > 1:
@@ -23,6 +22,22 @@ def main(process_id):
         fixed_fcid = fc.name.upper().replace("+", "-")
         if fixed_fcid.startswith("MS") and fixed_fcid.endswith("-50V2"):
             fixed_fcid = fixed_fcid[:-len("-50V2")] + "-050V2"
+        if fixed_fcid.startswith("MS"):
+            if not re.match(r"MS\d{7}-\d\d\dV\d", fixed_fcid):
+                print 'MiSeq reagent cartridge ID has the wrong format.'
+                sys.exit(1)
+        elif fixed_fcid.startswith("NS"):
+            if not re.match(r"NS\d{7}-REAGT", fixed_fcid):
+                print ""
+                sys.exit(1)
+        elif fixed_fcid.startswith("H"):
+            if len(fixed_fcid) != 9:
+                print "HiSeq flow cell ID should be 9 characters long."
+                sys.exit(1)
+        else:
+            print "The specified value {0} doesn't look like a valid reagent/flow cell ID.".format(fixed_fcid)
+            sys.exit(1)
+
         if fixed_fcid.startswith("RGT"):
             print 'RGT number "' + fc.name + '" shoud not be used as container ID. For MiSeq it should \
                     be MSxxxxx-nnnVn, for HiSeq it is printed on the flow cell package.'
