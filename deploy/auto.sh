200059fcf1572c370af6c45a04b1d47b5004c4e9
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2018-10-09 10:26:44 +0200

    site dependent deploy script

--- deploy/auto.sh
+++ deploy/auto.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
+if [ `hostname` == "sandbox-lims.sequencing.uio.no" ]
+then
+	SCRIPT=./deploy-cees.sh
+elif [ `hostname` == "dev-lims.sequencing.uio.no" ]
+then
+	SCRIPT=./deploy-ous.sh
+fi
 TAG=`date "+lims_%Y%m%d"$1`
 ./tag.sh $TAG
-./deploy-ous.sh $TAG
+$SCRIPT $TAG
