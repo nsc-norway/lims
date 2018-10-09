800f3ff61d02184e538e4437441ee825c7391337
Marius Bjoernstad <paalmbj@dev-lims.sequencing.uio.no>
2018-10-09 14:20:55 +0200

    Added check for TSD

--- reports/nels-project-report.py
+++ reports/nels-project-report.py
@@ -6,15 +6,17 @@ lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
 
 date_str = str(datetime.date.today() - datetime.timedelta(days=-1))
 timestamp_str = date_str + "T00:00:00Z"
-nels_projects = []
+nels_tsd_projects = []
 
 for project in lims.get_projects(last_modified=timestamp_str):
     if project.udf.get('Delivery method') == "NeLS project":
-        nels_projects.append((project.name, project.udf.get('NeLS project identifier', '--not provided--')))
+        nels_tsd_projects.append((project.name, project.udf.get('NeLS project identifier', '--not provided--')))
+    elif project.udf.get('Delivery method') == "TSD project":
+        nels_tsd_projects.append((project.name, "TSD"))
 
-if nels_projects:
-    print("*** New NeLS projects registered since {0} ***".format(date_str))
+if nels_tsd_projects:
+    print("*** New NeLS/TSD projects registered since {0} ***".format(date_str))
     print("Project name\t\tNeLS ID")
-    for project in nels_projects:
+    for project in nels_tsd_projects:
         print("{0}\t\t{1}".format(*project))
 
