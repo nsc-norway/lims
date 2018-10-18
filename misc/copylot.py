c624e12d16a3a9fbbd9b06f185082143a387e393
Marius Bjoernstad <paalmbj@dev-lims.sequencing.uio.no>
2018-10-18 14:19:18 +0200

    More general lot copy script

--- misc/copylot.py
+++ misc/copylot.py
@@ -3,25 +3,35 @@ from genologics import config
 import sys
 
 lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
+attrs = ["name", "lot_number", "created_date", "last_modified_date", "expiry_date", "created_by", "last_modified_by", "status", "notes"]
+
+if len(sys.argv) <= 3:
+    print("usage: copylot.py FROM_LOT TO_LOT LOTNAME")
+    sys.exit(1)
 
 def copy_lot(old_lot, new_kit):
     new_lot_data = {}
-    for attr in ["name", "lot_number", "created_date", "last_modified_date", "expiry_date", "created_by", "last_modified_by", "status", "notes"]:
+    for attr in attrs: 
         new_lot_data[attr] = getattr(old_lot, attr)
     new_lot_data['reagent_kit'] = new_kit
     ReagentLot.create(lims, **new_lot_data)
 
 #old_kit = lims.get_reagent_kits(name="Diag_HiSeq 2500 Cluster Kit 1/2")[0]
-new_kit = lims.get_reagent_kits(name="NSC_MiSeq Reagent Kit 2/2")[0]
-old_lots = lims.get_reagent_lots(kitname="Diag_MiSeq Reagent Kit 2/2")
+new_kit = lims.get_reagent_kits(name=sys.argv[2])[0]
+all_lots = lims.get_reagent_lots(kitname=sys.argv[1])
 
-copied = 0
-for lot in old_lots:
-    if lot.status =="ACTIVE" and lot.name.endswith("V3"):
-        copy_lot(lot, new_kit)
-        copied += 1
-
-print "Copied", copied, "lots"
-
-
+for lot in all_lots:
+    if any(lot.name.startswith(a) for a in sys.argv[3:]):
+        print("> Found lot")
+        for attr in attrs:
+            print("%20s: %s" % (attr, getattr(lot, attr)))
+        y = raw_input(">Enter y to copy this lot: ")
+        if y == "y":
+            copy_lot(lot, new_kit)
+            a = raw_input(">Copied. Archive it: ")
+            if a == "y":
+                lot.status="ARCHIVED"
+                lot.put()
+        else:
+            break
 
