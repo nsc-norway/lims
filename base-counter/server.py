51125bd41f490af889b00b458412b1d1bdf537b2
Marius Bjoernstad <paalmbj@dev-lims.sequencing.uio.no>
2019-12-19 12:47:32 +0100

    handle status update of cancelled runs

--- base-counter/server.py
+++ base-counter/server.py
@@ -217,6 +217,7 @@ class RunStatus(object):
         self.cycle_arrival = {} # (cycle, time) pairs
         self.finished = False
         self.start_cancelled = start_cancelled
+        self.cancelled = start_cancelled
 
     def set_metadata(self):
         try:
@@ -273,6 +274,30 @@ class RunStatus(object):
     def check_finished(self):
         return os.path.exists(os.path.join(self.run_dir, "RTAComplete.txt"))
 
+    def check_cancelled(self):
+        """Heuristic to determine if cancelled. If current cycle time is
+        more than 2x last cycle time."""
+
+        if len(self.cycle_arrival) <= 1 and self.start_cancelled:
+            return True
+        if (not self.finished) and len(self.cycle_arrival) > 0:
+            current_cycle_time = time.time() - self.cycle_arrival[self.current_cycle]
+            if current_cycle_time > 25*3600 or (SEQUENCERS[self.machine_id][0] != 'hiseq' and current_cycle_time > 3600*7):
+                return True
+            if len(self.cycle_arrival) > 2:
+                cycle_rate, cycle_stride = self.get_cycle_rate()
+                next_index_cycles = sum(
+                        1
+                        for i in range(self.current_cycle+1, self.total_cycles)
+                        if self.data_cycles_lut[i] == self.data_cycles_lut[self.current_cycle]
+                        )
+                after_index_read = 0
+                if next_index_cycles:
+                    after_index_read = 25 # Some slack on start of read 2, takes about 25 cycles before writing on MiSeq
+                cancelled = current_cycle_time > (6+after_index_read+next_index_cycles) * (cycle_stride / cycle_rate)
+                return cancelled
+        return False
+
     def update(self):
         if self.finished:
             return
@@ -297,6 +322,10 @@ class RunStatus(object):
 
         if self.check_finished():
             self.finished = True
+            updated = True
+        new_cancelled = self.check_cancelled()
+        if new_cancelled != self.cancelled:
+            self.cancelled = new_cancelled
             updated = True
         if updated:
             if self.clusters != 0 or initial_update:
@@ -358,32 +387,6 @@ class RunStatus(object):
             return self.rate * (time.time() - self.start_time)
         else:
             return 0
-
-    @property
-    def cancelled(self):
-        """Heuristic to determine if cancelled. If current cycle time is
-        more than 2x last cycle time."""
-
-        if len(self.cycle_arrival) <= 1 and self.start_cancelled:
-            return True
-        if (not self.finished) and len(self.cycle_arrival) > 0:
-            current_cycle_time = time.time() - self.cycle_arrival[self.current_cycle]
-            if current_cycle_time > 25*3600 or (SEQUENCERS[self.machine_id][0] != 'hiseq' and current_cycle_time > 3600*7):
-                return True
-            if len(self.cycle_arrival) > 2:
-                cycle_rate, cycle_stride = self.get_cycle_rate()
-                next_index_cycles = sum(
-                        1
-                        for i in range(self.current_cycle+1, self.total_cycles)
-                        if self.data_cycles_lut[i] == self.data_cycles_lut[self.current_cycle]
-                        )
-                after_index_read = 0
-                if next_index_cycles:
-                    after_index_read = 15 # Some slack on start of read 2, takes about 15 cycles before writing on MiSeq
-                cancelled = current_cycle_time > (6+after_index_read+next_index_cycles) * (cycle_stride / cycle_rate)
-                return cancelled
-
-        return False
 
     @property
     def data_package(self):
