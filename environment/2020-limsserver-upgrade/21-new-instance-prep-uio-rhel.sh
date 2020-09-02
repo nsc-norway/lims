c288f88a1252438a7a7ad64c1af4b9cc897fed7c
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2020-09-02 17:23:10 +0200

    Set commands as run

--- environment/2020-limsserver-upgrade/21-new-instance-prep-uio-rhel.sh
+++ environment/2020-limsserver-upgrade/21-new-instance-prep-uio-rhel.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
-yum install -y postgresql96-server
-#/usr/pgsql-9.6/bin/postgresql96-setup initdb
-#systemctl enable postgresql-9.6
-#systemctl start postgresql-9.6
+sudo yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+sudo yum install -y postgresql96-server
+sudo /usr/pgsql-9.6/bin/postgresql96-setup initdb
+sudo systemctl enable postgresql-9.6
+sudo systemctl start postgresql-9.6
+
+# Edit: /var/lib/pgsql/9.6/data/postgresql.conf
+# SET shared_buffers = 6 GB
+
+# Edit: /var/lib/pgsql/9.6/data/pg_hba.conf
+# CHANGE "ident" to "md5"
+### # IPv4 local connections:
+### host    all             all             127.0.0.1/32            md5
+### # IPv6 local connections:
+### host    all             all             ::1/128                 md5
