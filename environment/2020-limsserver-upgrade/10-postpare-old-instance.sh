bd77145b1a8567c37fe0c36522e3c2c7b5fd5eb1
P}l Marius Bj|rnstad <paalmbj@sandbox-lims.sequencing.uio.no>
2020-09-16 15:03:31 +0200

    Amend notes about decommissioning instance

--- environment/2020-limsserver-upgrade/10-postpare-old-instance.sh
+++ environment/2020-limsserver-upgrade/10-postpare-old-instance.sh
@@ -2,6 +2,12 @@
 
 # Run as the root user on the old clarity server before upgrade
 
+
+# -- 1. validation ---
+
+# The Pre-validation RPM for the new version should be installed here.
+# (this is not usually the case, we have to add the repo file explicitly)
+# Install PreValidation 5.2:
 yum --enablerepo=* install ClarityLIMS-UpgradePreValidation
 bash /opt/gls/ClarityUpgradeValidation/bin/validate.sh
 yum remove ClarityLIMS-UpgradePreValidation
@@ -9,7 +15,14 @@ yum remove ClarityLIMS-UpgradePreValidation
 
 /opt/gls/clarity/bin/run_clarity.sh stop
 
-pg_dump -U <database_user> -b -O -Ft clarityDB -f ~glsjboss/backups/database/clarity-old_version-`date +%Y%m%d%H%M`.tar
+# pg_dump doesn't seem to respect TMPDIR var. Instead we create /opt/tmp and bind-mount it for the
+# duration of the database backup.
+mkdir -p /opt/tmp
+chown postgres:postgres /opt/tmp
+chmod 2700 /opt/tmp
+mount -o bind /opt/tmp /tmp
+sudo -u postgres bash -c 'pg_dump -b -O -Ft clarityDB' | gzip > /opt/clarity-5.1-`date +%Y%m%d%H%M`.tar
+umount /tmp
 
 cd /
 rpm -qa | grep "BaseSpace\|Clarity" > clarityrpms.txt
@@ -22,4 +35,4 @@ tar cfJ /opt/backups.tar.xz \
     /var/lib/pgsql/9.6/data/pg_hba.conf \
     /var/lib/pgsql/9.6/data/postgresql.conf \
     clarityrpms.txt \
-    DATABASE_FILE_TODELOO
+    /opt/clarity-5.1-*.tar
