ca5877fc0a217213389f8d2556f9cafb11706fe7
Marius Bjoernstad <marius.bjornstad@medisin.uio.no>
2020-09-23 11:50:25 +0200

    Describe nextseq configuration

---
+++ environment/2020-limsserver-upgrade/61-configure-nextseq-integration.sh
@@ -0,0 +1,23 @@
+
+
+# 1.	Install NextSeq package:
+yum --enablerepo=GLS_Clarity_NextSeq install ClarityLIMS-Illumina-NextSeq-Package-v2
+sudo -u glsjboss /opt/gls/clarity/config/configure_extensions_nextseq_sequencingservice-v2.sh
+
+
+# 2. Add property for the process name
+sudo -u glsjboss /opt/gls/clarity/bin/java -jar /opt/gls/clarity/tools/propertytool/omxprops-ConfigTool.jar addPropertyType nextseq.v2.seqservice.sequenceProcessBaseName "NextSeq 500/550 Run" "Sequencing process type base display name for partial term matching."
+
+# 3. Set properties
+cd /opt/gls/clarity/tools/propertytool
+HOST=`hostname -f`
+sudo -i -u glsjboss java -jar /opt/gls/clarity/tools/propertytool/omxprops-ConfigTool.jar set -y -f $HOST  nextseq.v2.seqservice.eventFileDirectory.1 '/boston/runScratch/gls_events_neseq'
+sudo -i -u glsjboss java -jar /opt/gls/clarity/tools/propertytool/omxprops-ConfigTool.jar set -y -f $HOST  nextseq.v2.seqservice.eventFileDirectorySuffixes '1'
+sudo -i -u glsjboss java -jar /opt/gls/clarity/tools/propertytool/omxprops-ConfigTool.jar set -y -f $HOST  nextseq.v2.seqservice.netPathPrefixReplace.1 '/boston/runScratch'
+sudo -i -u glsjboss java -jar /opt/gls/clarity/tools/propertytool/omxprops-ConfigTool.jar set -y -f $HOST  nextseq.v2.seqservice.netPathPrefixSearch.1 '\\boston.nscamg.local\runScratch'
+sudo -i -u glsjboss java -jar /opt/gls/clarity/tools/propertytool/omxprops-ConfigTool.jar set -y -f $HOST  nextseq.v2.seqservice.netPathPrefixSearchReplaceSuffixes '1'
+sudo -i -u glsjboss java -jar /opt/gls/clarity/tools/propertytool/omxprops-ConfigTool.jar set -y -f $HOST  nextseq.v2.seqservice.sequenceProcessBaseName 'NextSeq 500/550 Run'
+
+# 5. Enable & Start service
+sudo systemctl enable nextseq_seqservice-v2
+sudo systemctl start nextseq_seqservice-v2
