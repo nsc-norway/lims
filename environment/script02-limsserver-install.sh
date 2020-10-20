d2f8e5a32ea9ca63314de82695817caa7ebde322
Marius Bjoernstad <marius.bjornstad@medisin.uio.no>
2020-10-20 17:13:44 +0200

    Update the install script

--- environment/script02-limsserver-install.sh
+++ environment/script02-limsserver-install.sh
@@ -4,19 +4,20 @@
 
 set -e
 yum install python2 python3
+rm -fr packages/*
 tar xfz packages.tar.gz
 pip3 install --no-index --find-links packages/nsc-python36 virtualenv
 
 /usr/local/bin/virtualenv -p python2 /opt/nsc/envs/nsc-python27
 source /opt/nsc/envs/nsc-python27/bin/activate
-pip install --no-index --find-links packages/nsc-python27/ -r requirements-py27-origin.txt
+pip install --upgrade --no-index --find-links packages/nsc-python27/ -r requirements-py27-origin.txt
 deactivate
 
 python3 -m venv /opt/nsc/envs/nsc-python36
 source /opt/nsc/envs/nsc-python36/bin/activate
-pip install packages/nsc-python36/pip-*-py2.py3-none-any.whl
+pip install --upgrade packages/nsc-python36/pip-*-py2.py3-none-any.whl
 #pip install --no-index --find-links --upgrade pip
-pip install --no-index --find-links packages/nsc-python36/ -r requirements-py36-origin.txt
+pip install --upgrade --no-index --find-links packages/nsc-python36/ -r requirements-py36-origin.txt
 deactivate
 
 ln -sf /opt/nsc/envs/nsc-python27/bin/python /usr/bin/nsc-python27
