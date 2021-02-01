13c0001f13dd083ddf4418925b6372fdba5fa7e0
P}l Marius Bj|rnstad <paalmbj@sandbox-lims.sequencing.uio.no>
2021-02-01 21:28:09 +0100

    Only try to route 'Analytes', which are the only kind of output that should be routed

--- helpers/assign-outputs-prefix-workflow.py
+++ helpers/assign-outputs-prefix-workflow.py
@@ -14,7 +14,8 @@ def main(process_id, workflow_prefix):
         workflow_data = lims.get_workflows(add_info=True)
         for workflow, info in zip(*workflow_data):
             if info['status'] == "ACTIVE" and info['name'].startswith(workflow_prefix):
-                lims.route_analytes(process.all_outputs(unique=True), workflow)
+                output_samples = [o['uri'] for i, o in process.input_output_maps if o['output-type'] == "Analyte"]
+                lims.route_analytes(output_samples, workflow)
                 break
         else: # If not breaked
             print(('Error: No workflow matching "' + str(workflow_prefix) + '" was found.').encode('utf-8'))
