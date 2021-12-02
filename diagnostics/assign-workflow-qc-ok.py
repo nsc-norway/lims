dc551ab7addb6a8f9909f7ea4e0719655f3ebe18
Marius Bjørnstad <pmb@fa2k.net>
2021-12-02 14:46:59 +0100

    Update script for new MiSeq type, and cleanup

--- diagnostics/assign-workflow-qc-ok.py
+++ diagnostics/assign-workflow-qc-ok.py
@@ -8,38 +8,54 @@ from genologics import config
 def main(process_id):
     lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
     process = Process(lims, id=process_id)
+    # Get inputs of demultiplexing step (process)
     inputs = list(set(i['uri'].stateless for i, o in process.input_output_maps
                     if o['output-generation-type'] == "PerReagentLabel"))
-    lims.get_batch(inputs)
-    lims.get_batch(sample for input in inputs for sample in input.samples)
-    qc_results = {}
-    if any(i.qc_flag == "UNKNOWN" for i in inputs):
-        for typ in ["NovaSeq Data QC", "NextSeq 500/550 Run NSC 3.0"]:
-            try:
-                qc_list = [qc.stateless for qc in lims.get_qc_results_re(inputs, typ)]
+    
+    diag_inputs = []
+    for inp in inputs:
+        # Check if the first sample that's not a control sample has Diag project
+        for sample in inp.samples:
+            if sample.project:
+                if sample.project.udf.get('Project type') == "Diagnostics":
+                    diag_inputs.append(inp)
                 break
-            except KeyError: # If missing QC result, get_qc_results_re throws KeyError
-                qc_list = []
-        if qc_list:
-            lims.get_batch(qc_list)
-            qc_results = dict(zip(inputs, qc_list))
-    routables = []
-    for i in inputs:
-        if i.qc_flag == "PASSED" or (
-                i in qc_results and qc_results[i].qc_flag == "PASSED"
-                ):
-            if i.samples[0].project.udf.get('Project type') == "Diagnostics":
-                routables += [sample.artifact for sample in i.samples]
-    if routables:
-        workflows = lims.get_workflows()
-        match_workflows = [] # Contains version, then workflow object
-        for w in workflows:
-            # This will do a GET for each workflow in the system. Performance is bad.
-            m = re.match(r"processing of hts-data diag (\d)\.(\d)", w.name, re.IGNORECASE)
-            if w.status == "ACTIVE" and m:
-                match_workflows.append((int(m.group(1)), int(m.group(2)), w))
-        workflow = sorted(match_workflows)[-1]
-        lims.route_analytes(routables, workflow[2])
+
+    if diag_inputs:
+        # Find MiSeq Run / NextSeq Run / NovaSeq Data QC
+        qc_proc = None
+        for qc_proc_cand in lims.get_processes(inputartifactlimsid=[a.id for a in diag_inputs]):
+            # Get the first output with 'PerInput'
+            for i, o in qc_proc_cand.input_output_maps:
+                if o['output-generation-type'] == 'PerInput':
+                    if o['uri'].stateless.qc_flag != "UNKNOWN":
+                        # This is a QC process, good enough
+                        qc_proc = qc_proc_cand
+                    break
+            if qc_proc:
+                break
+
+        # Find the artifacts to route (put in dict to de-duplicate)
+        routables = {}
+        if qc_proc:
+            for inp in diag_inputs:
+                # Find the QC state of this lane
+                for i, o in qc_proc.input_output_maps:
+                    if o['output-generation-type'] == 'PerInput' and i['limsid'] == inp.id:
+                        if o['uri'].stateless.qc_flag == 'PASSED':
+                            for sam in lims.get_batch(inp.samples):
+                                routables[sam.artifact.id] = sam.artifact
+
+        if routables:
+            workflows = lims.get_workflows()
+            match_workflows = [] # Contains version, then workflow object
+            for w in workflows:
+                # This will do a GET for each workflow in the system. Performance is bad.
+                m = re.match(r"processing of hts-data diag (\d)\.(\d)", w.name, re.IGNORECASE)
+                if w.status == "ACTIVE" and m:
+                    match_workflows.append((int(m.group(1)), int(m.group(2)), w))
+            workflow = sorted(match_workflows)[-1]
+            lims.route_analytes(routables.values(), workflow[2])
 
 if __name__ == "__main__":
     main(sys.argv[1])
